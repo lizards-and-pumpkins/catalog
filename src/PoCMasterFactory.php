1332ec6dc6cdaa3eab56221b828a1a7ce2fb4006
Vinai Kopp <vinai@netzarbeiter.com>
2014-10-15 13:45:05 +0200

    Issue #1: Add @method annotations to the PoCMasterFactory

--- src/PoCMasterFactory.php
+++ src/PoCMasterFactory.php
@@ -3,7 +3,17 @@
 
 namespace Brera\PoC;
 
-
+/**
+ * Interface PoCMasterFactory
+ * @package Brera\PoC
+ *
+ * @method DataPoolWriter createDataPoolWriter
+ * @method DataPoolReader createDataPoolReader
+ * @method ProductSeoUrlRouter createProductSeoUrlRouter
+ * @method ProductRepository getProductRepository
+ * @method DomainEventQueue getEventQueue
+ * @method DomainEventConsumer createDomainEventConsumer
+ */
 class PoCMasterFactory implements MasterFactory
 {
     use MasterFactoryTrait;
