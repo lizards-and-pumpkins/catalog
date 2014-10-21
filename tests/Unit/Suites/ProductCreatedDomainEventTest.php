f43eb6c75fd1efa9207a1e12254dde8ae9993728
Tim Bezhashvyly <tim.bezhashvyly@gmail.com>
2014-10-21 19:42:47 +0200

    Issue #3: Fix XML in ProductImportDomainEvent test

--- tests/Unit/Suites/ProductCreatedDomainEventTest.php
+++ tests/Unit/Suites/ProductCreatedDomainEventTest.php
@@ -12,7 +12,7 @@ class ProductImportDomainEventTest extends \PHPUnit_Framework_TestCase
 	 */
 	public function itShouldReturnProductImportXml()
 	{
-		$xml = '<?xml version="1.0"><rootNode></rootNode>';
+		$xml = '<?xml version="1.0"?><rootNode></rootNode>';
 
 		$domainEvent = new ProductImportDomainEvent($xml);
 		$result = $domainEvent->getXml();
