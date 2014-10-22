cad64f673818c84857b9c82875881b885ed71b3a
Tim Bezhashvyly <tim.bezhashvyly@gmail.com>
2014-10-22 15:01:01 +0200

    Issue #1: Add PoCProductRenderer test

---
+++ tests/Unit/Suites/Renderer/PoCProductRendererTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Brera\PoC\Renderer;
+
+use Brera\PoC\Product\Product;
+
+/**
+ * @covers \Brera\PoC\Renderer\PoCProductRenderer
+ */
+class PoCProductRendererTest extends \PHPUnit_Framework_TestCase
+{
+	/**
+	 * @test
+	 */
+	public function itShouldReturnAnHtmlString()
+	{
+		$stubProductId = '123';
+		$stubProductName = 'Name';
+		$expectation = '<p>' . $stubProductId . ': ' . $stubProductName . '</p>';
+
+		$stubProduct = $this->getMockBuilder(Product::class)
+			->disableOriginalConstructor()
+			->getMock();
+
+		$stubProduct->expects($this->once())
+			->method('getId')
+			->willReturn($stubProductId);
+		$stubProduct->expects($this->once())
+			->method('getName')
+			->willReturn($stubProductName);
+
+		$renderer = new PoCProductRenderer();
+		$result = $renderer->render($stubProduct);
+
+		$this->assertEquals($expectation, $result);
+	}
+}
