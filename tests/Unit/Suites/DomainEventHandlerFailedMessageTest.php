4469750947e2ecda10c63953b3f1c14f4063b823
Tim Bezhashvyly <tim.bezhashvyly@gmail.com>
2014-10-23 10:51:42 +0200

    Issue #1: Add DomainEventHandlerFailedMessage test

---
+++ tests/Unit/Suites/DomainEventHandlerFailedMessageTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Brera\PoC;
+
+/**
+ * @covers \Brera\PoC\DomainEventHandlerFailedMessage
+ */
+class DomainEventHandlerFailedMessageTest extends \PHPUnit_Framework_TestCase
+{
+	/**
+	 * @var \PHPUnit_Framework_MockObject_MockObject
+	 */
+	private $stubDomainEvent;
+
+	/**
+	 * @var \PHPUnit_Framework_MockObject_MockObject
+	 */
+	private $stubException;
+
+	/**
+	 * @var DomainEventHandlerFailedMessage
+	 */
+	private $message;
+
+	protected function setUp()
+	{
+		$this->stubDomainEvent = $this->getMock(DomainEvent::class);
+		$this->stubException = $this->getMock(\Exception::class);
+
+		$this->message = new DomainEventHandlerFailedMessage($this->stubDomainEvent, $this->stubException);
+	}
+
+	/**
+	 * @test
+	 */
+	public function itShouldReturnDomainEvent()
+	{
+		$result = $this->message->getDomainEvent();
+		$this->assertInstanceOf(DomainEvent::class, $result);
+	}
+
+	/**
+	 * @test
+	 */
+	public function itShouldReturnAnException()
+	{
+		$result = $this->message->getException();
+		$this->assertInstanceOf(\Exception::class, $result);
+	}
+}
