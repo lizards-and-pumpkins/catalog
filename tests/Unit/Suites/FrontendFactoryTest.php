89d26440657dc37dd1583675f40f39fa160a8acf
Tim Bezhashvyly <tim.bezhashvyly@gmail.com>
2015-01-11 23:10:48 +0100

    Issue #1: Add applicable FrontendFactory tests

---
+++ tests/Unit/Suites/FrontendFactoryTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Brera;
+
+use Brera\Product\CatalogImportApiRequestHandler;
+
+/**
+ * @covers \Brera\FrontendFactory
+ * @covers \Brera\FactoryTrait
+ */
+class FrontendFactoryTest extends \PHPUnit_Framework_TestCase
+{
+	/**
+	 * @var FrontendFactory
+	 */
+	private $frontendFactory;
+
+	public function setUp()
+	{
+		$this->frontendFactory = new FrontendFactory();
+	}
+
+	/**
+	 * @test
+	 * @expectedException \Brera\NoMasterFactorySetException
+	 */
+	public function itShouldThrowAnExceptionIfNoMasterFactoryIsSet()
+	{
+		/*
+		 * The getMasterFactory method is protected so other method which will trigger it is called.
+		 */
+		$this->frontendFactory->createApiRouter();
+	}
+
+	/**
+	 * @test
+	 */
+	public function itShouldReturnCatalogImportApiRequestHandler()
+	{
+		$result = $this->frontendFactory->createCatalogImportApiRequestHandler();
+
+		$this->assertInstanceOf(CatalogImportApiRequestHandler::class, $result);
+	}
+}
