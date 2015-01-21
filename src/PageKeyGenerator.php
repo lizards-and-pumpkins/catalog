a521587cd60fec85d19ede51d43277b1a530cbe7
Fabian Blechschmidt <blechschmidt@fabian-blechschmidt.de>
2015-01-21 10:16:54 +0100

    Issue #26: Implement basic page key generator for snippet and snippet list

---
+++ src/PageKeyGenerator.php
@@ -0,0 +1,51 @@
+<?php
+
+
+namespace Brera;
+
+
+class PageKeyGenerator
+{
+
+    /**
+     * @todo logic needs to be implemented and overthink
+     * @todo we need somehow to define which information goes into this first key
+     * @todo I like the idea to only add the version and everything else can be
+     * @todo done in this first snippet, so it can be just "empty" only containing one palceholder
+     *
+     * @param string      $url
+     * @param Environment $env
+     *
+     * @return string
+     */
+    public function getKeyForSnippet($url, Environment $env)
+    {
+        // $path contains the starting /
+        return $this->getKey($url, $env);
+    }
+
+    /**
+     * @param string      $url
+     * @param Environment $env
+     *
+     * @return string
+     */
+    public function getKeyForSnippetList($url, Environment $env)
+    {
+        return $this->getKey($url, $env) . '_l';
+    }
+
+    /**
+     * @param             $url
+     * @param Environment $env
+     *
+     * @return mixed|string
+     */
+    private function getKey($url, Environment $env)
+    {
+        $path = parse_url($url, PHP_URL_PATH);
+        $key = preg_replace('#[^a-zA-Z0-9]#', '_', $path);
+        $key .= '_' . $env->getVersion();
+        return $key;
+    }
+}
---
+++ tests/Unit/Suites/PageKeyGeneratorTest.php
@@ -0,0 +1,65 @@
+<?php
+
+
+namespace Brera;
+
+
+class PageKeyGeneratorTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var PageKeyGenerator
+     */
+    private $pageKeyGenerator;
+
+    protected function setUp()
+    {
+        $this->pageKeyGenerator = new PageKeyGenerator();
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldReturnStrings()
+    {
+        $env = new VersionedEnvironment(DataVersion::fromVersionString('1'));
+        $url = 'http://localhost/product.html';
+
+        $this->assertInternalType(
+            'string',
+            $this->pageKeyGenerator->getKeyForSnippet($url, $env)
+        );
+
+        $this->assertInternalType(
+            'string',
+            $this->pageKeyGenerator->getKeyForSnippetList($url, $env)
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldGenerateAKeyForSnippetFromAnEnvironmentAndUrl()
+    {
+        $env = new VersionedEnvironment(DataVersion::fromVersionString('1'));
+        $url = 'http://localhost/product.html';
+
+        $this->assertEquals(
+            '_product_html_1',
+            $this->pageKeyGenerator->getKeyForSnippet($url, $env)
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldGenerateAKeyForSnippetListFromAnEnvironmentAndUrl()
+    {
+        $env = new VersionedEnvironment(DataVersion::fromVersionString('1'));
+        $url = 'http://localhost/product.html';
+
+        $this->assertEquals(
+            '_product_html_1_l',
+            $this->pageKeyGenerator->getKeyForSnippetList($url, $env)
+        );
+    }
+}
