42f85e11ac9ea8224971b3293a396813964e469f
Fabian Blechschmidt <blechschmidt@fabian-blechschmidt.de>
2015-01-21 15:17:20 +0100

    Issue #26: comments and formatting

--- tests/Unit/Suites/KeyValue/AbstractDataPool.php
+++ tests/Unit/Suites/KeyValue/AbstractDataPool.php
@@ -2,68 +2,89 @@
 
 namespace Brera\KeyValue;
 
-use Brera\Product\ProductId;
 use Brera\Http\HttpUrl;
+use Brera\Product\ProductId;
 
 class AbstractDataPool extends \PHPUnit_Framework_TestCase
 {
-	/**
-	 * @var KeyValueStore|\PHPUnit_Framework_MockObject_MockObject
-	 */
-	protected $stubKeyValueStore;
+    /**
+     * @var KeyValueStore|\PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $stubKeyValueStore;
 
-	/**
-	 * @var KeyValueStoreKeyGenerator|\PHPUnit_Framework_MockObject_MockObject
-	 */
-	protected $stubKeyGenerator;
+    /**
+     * @var KeyValueStoreKeyGenerator|\PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $stubKeyGenerator;
 
-	protected function setUp()
-	{
-		$this->stubKeyValueStore = $this->getMock(KeyValueStore::class);
-		$this->stubKeyGenerator = $this->getMock(KeyValueStoreKeyGenerator::class);
-	}
+    /**
+     *
+     */
+    protected function setUp()
+    {
+        $this->stubKeyValueStore = $this->getMock(KeyValueStore::class);
+        $this->stubKeyGenerator = $this->getMock(KeyValueStoreKeyGenerator::class);
+    }
 
-	protected function getStubProductId()
-	{
-		$productId = $this->getMockBuilder(ProductId::class)
-			->disableOriginalConstructor()
-			->getMock();
+    /**
+     * @return \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected function getStubProductId()
+    {
+        $productId = $this->getMockBuilder(ProductId::class)
+            ->disableOriginalConstructor()
+            ->getMock();
 
-		return $productId;
-	}
+        return $productId;
+    }
 
-	protected function addSetMethodToStubKeyValueStore()
-	{
-		$this->stubKeyValueStore->expects($this->once())
-			->method('set');
-	}
+    /**
+     *
+     */
+    protected function addSetMethodToStubKeyValueStore()
+    {
+        $this->stubKeyValueStore->expects($this->once())
+            ->method('set');
+    }
 
-	protected function addGetMethodToStubKeyValueStore($returnValue)
-	{
-		$this->stubKeyValueStore->expects($this->once())
-			->method('get')
-			->willReturn($returnValue);
-	}
+    /**
+     * @param $returnValue
+     */
+    protected function addGetMethodToStubKeyValueStore($returnValue)
+    {
+        $this->stubKeyValueStore->expects($this->once())
+            ->method('get')
+            ->willReturn($returnValue);
+    }
 
-	protected function addHasMethodToStubKeyValueStore($returnResult)
-	{
-		$this->stubKeyValueStore->expects($this->once())
-			->method('has')
-			->willReturn($returnResult);
-	}
+    /**
+     * @param $returnResult
+     */
+    protected function addHasMethodToStubKeyValueStore($returnResult)
+    {
+        $this->stubKeyValueStore->expects($this->once())
+            ->method('has')
+            ->willReturn($returnResult);
+    }
 
-	protected function addStubMethodToStubKeyGenerator($method)
-	{
-		$this->stubKeyGenerator->expects($this->once())
-			->method($method)
-			->willReturn('dummy_key');
-	}
+    /**
+     * @param $method
+     */
+    protected function addStubMethodToStubKeyGenerator($method)
+    {
+        $this->stubKeyGenerator->expects($this->once())
+            ->method($method)
+            ->willReturn('dummy_key');
+    }
 
-	protected function getDummyUrl()
-	{
-		$urlString = 'http://example.com/path';
-		$url = HttpUrl::fromString($urlString);
+    /**
+     * @return HttpUrl
+     */
+    protected function getDummyUrl()
+    {
+        $urlString = 'http://example.com/path';
+        $url = HttpUrl::fromString($urlString);
 
-		return $url;
-	}
+        return $url;
+    }
 }
