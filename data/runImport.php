5d0e8849745256dda276e1faa928e654c98680d1
Tim Bezhashvyly <tim.bezhashvyly@gmail.com>
2015-01-31 09:39:45 +0100

    Issue #50: Create temporary CLI script which will trigger catalog import

---
+++ data/runImport.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once '../vendor/autoload.php';
+
+chdir(__DIR__ . '/..');
+
+use Brera\Product\CatalogImportDomainEvent;
+use Brera\PoCMasterFactory;
+use Brera\SampleFactory;
+
+$factory = new PoCMasterFactory();
+$factory->register(new SampleFactory());
+
+$xml = file_get_contents('sample-data/product.xml');
+
+$queue = $factory->getEventQueue();
+$queue->add(new CatalogImportDomainEvent($xml));
+
+$consumer = $factory->createDomainEventConsumer();
+$numberOfMessages = 3;
+$consumer->process($numberOfMessages);
