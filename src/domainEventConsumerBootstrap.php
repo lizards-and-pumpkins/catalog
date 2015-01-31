6c383326dfc969f5454fa563a6591a5da4cdcb1c
Tim Bezhashvyly <tim.bezhashvyly@gmail.com>
2015-01-31 09:38:56 +0100

    Issue #50: Remove obsolete domainEventConsumerBootstrap

--- src/domainEventConsumerBootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Brera;
-
-// Use a "real" factory and then this is the consumer bootstrap code
-$factory = new IntegrationTestFactory();
-$consumer = $factory->createDomainEventConsumer();
-$consumer->process(1);
