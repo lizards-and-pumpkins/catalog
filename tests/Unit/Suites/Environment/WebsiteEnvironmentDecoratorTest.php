60f35b3ed5748b1bdf6fca1a4281596ee561f9d1
Vinai Kopp <vinai@netzarbeiter.com>
2015-02-08 17:14:58 +0100

    Issue #53: Create WebsiteEnvironmentDecorator

---
+++ tests/Unit/Suites/Environment/WebsiteEnvironmentDecoratorTest.php
@@ -0,0 +1,39 @@
+<?php
+
+
+namespace Brera\Environment;
+
+require __DIR__ . '/EnvironmentDecoratorTestAbstract.php';
+
+/**
+ * @covers \Brera\Environment\WebsiteEnvironmentDecorator
+ * @covers \Brera\Environment\EnvironmentDecorator
+ */
+class WebsiteEnvironmentDecoratorTest extends EnvironmentDecoratorTestAbstract
+{
+    /**
+     * @return string
+     */
+    protected function getDecoratorUnderTestCode()
+    {
+        return WebsiteEnvironmentDecorator::CODE;
+    }
+
+    /**
+     * @return array
+     */
+    protected function getStubEnvironmentData()
+    {
+        return [$this->getDecoratorUnderTestCode() => 'test-website-code'];
+    }
+    
+    /**
+     * @param Environment $stubEnvironment
+     * @param array $stubEnvironmentData
+     * @return WebsiteEnvironmentDecorator
+     */
+    protected function createEnvironmentDecoratorUnderTest(Environment $stubEnvironment, array $stubEnvironmentData)
+    {
+        return new WebsiteEnvironmentDecorator($stubEnvironment, $stubEnvironmentData);
+    }
+}
