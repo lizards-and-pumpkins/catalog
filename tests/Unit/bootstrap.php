f6e91155d3cf1402633292140bc0adf05f4ddb9b
Vinai Kopp <vinai@netzarbeiter.com>
2015-02-09 22:38:57 +0100

    Issue #75: Add PSR-4 compatible autoloader to unit test bootstrap so classes can be PSR-2 compliant

--- tests/Unit/bootstrap.php
+++ tests/Unit/bootstrap.php
@@ -3,3 +3,22 @@
 chdir(__DIR__ . '/../..');
 
 require_once __DIR__ . '/../../vendor/autoload.php';
+
+// Closure autoloader from
+// https://github.com/php-fig/fig-standards/blob/master/accepted/PSR-4-autoloader-examples.md#closure-example
+
+spl_autoload_register(function ($class) {
+    $prefix = 'Brera\\';
+    $len = strlen($prefix);
+    if (strncmp($prefix, $class, $len) !== 0) {
+        return;
+    }
+
+    $relativeClass = substr($class, $len);
+
+    $file = __DIR__ . '/Suites/' . str_replace('\\', '/', $relativeClass) . '.php';
+
+    if (file_exists($file)) {
+        require $file;
+    }
+});
