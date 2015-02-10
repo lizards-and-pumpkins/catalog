96b8bd5c138e8211a7d0a5d377df5b4801a13e11
Vinai Kopp <vinai@netzarbeiter.com>
2015-02-10 22:33:24 +0100

    Issue #73: Return HttpResponse instead of string, as specified by interface

--- src/Product/CatalogImportApiRequestHandler.php
+++ src/Product/CatalogImportApiRequestHandler.php
@@ -2,12 +2,16 @@
 
 namespace Brera\Product;
 
+use Brera\DefaultHttpResponse;
 use Brera\Http\HttpRequestHandler;
 
 class CatalogImportApiRequestHandler implements HttpRequestHandler
 {
     public function process()
     {
-        return json_encode('dummy response');
+        // todo: change to json response
+        $response = new DefaultHttpResponse();
+        $response->setBody(json_encode('dummy response'));
+        return $response;
     }
 }
