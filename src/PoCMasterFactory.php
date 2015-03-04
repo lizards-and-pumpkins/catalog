0489336a591b26e6c67dc8621ea18d199bc3a14c
Tim Bezhashvyly <tim.bezhashvyly@gmail.com>
2015-03-04 21:31:07 +0100

    Issue #134: Fix master factory annotations

--- src/PoCMasterFactory.php
+++ src/PoCMasterFactory.php
@@ -6,14 +6,12 @@ namespace Brera;
  * @method DataPool\DataPoolWriter createDataPoolWriter
  * @method DataPool\DataPoolReader createDataPoolReader
  * @method Queue\Queue getEventQueue
- * @method InMemoryLogger getLogger
  * @method Context\ContextBuilder createContextBuilder
  * @method Context\ContextBuilder createContextBuilderWithVersion
  * @method Context\ContextSourceBuilder createContextSourceBuilder
  * @method DomainEventConsumer createDomainEventConsumer
  * @method SnippetKeyGeneratorLocator getSnippetKeyGeneratorLocator
  * @method Logger getLogger
- * @method ContextSourceBuilder createContextSourceBuilder
  * @method Product\ProductDetailViewSnippetKeyGenerator createProductDetailViewSnippetKeyGenerator
  * @method GenericSnippetKeyGenerator createProductListingSnippetKeyGenerator
  */
