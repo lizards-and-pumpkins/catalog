48f85daaa95423df156b2b61cfcf069293fedf9c
Tim Bezhashvyly <tim.bezhashvyly@gmail.com>
2015-03-24 13:56:44 +0100

    Issue #184: Add product listing page integration test

---
+++ tests/Integration/Suites/ProductListingTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Brera;
+
+use Brera\Context\VersionedContext;
+use Brera\DataPool\DataPoolReader;
+use Brera\DataPool\KeyValue\InMemory\InMemoryKeyValueStore;
+use Brera\DataPool\SearchEngine\InMemorySearchEngine;
+use Brera\Http\HttpUrl;
+use Brera\Product\ProductListingRequestHandler;
+
+class ProductListingTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function itShouldReturnProductListingPageHtml()
+    {
+        $keyValueStore = new InMemoryKeyValueStore();
+        $searchEngine = new InMemorySearchEngine();
+
+        $dataPoolReader = new DataPoolReader($keyValueStore, $searchEngine);
+
+        $keyValueStore->set('_men-accessories_v:1_0', '{"source_id":"","root_snippet_code":"product_listing","page_snippet_codes":["product_in_listing_118235-251_language:de_DE_website:ru"]}');
+        $keyValueStore->set('product_listing__v:1.0', '<div>{{snippet product_1}}</div>');
+        $keyValueStore->set('product_in_listing_118235-251_language:de_DE_website:ru__v:1.0', 'bar');
+
+        $url = HttpUrl::fromString('http://example.com/men-accessories');
+        $context = new VersionedContext(DataVersion::fromVersionString('1.0'));
+        $snippetKeyGeneratorLocator = new SnippetKeyGeneratorLocator();
+        $urlPathKeyGenerator = new PoCUrlPathKeyGenerator();
+        $logger = new InMemoryLogger();
+
+        $pageBuilder = new ProductListingRequestHandler(
+            $url,
+            $context,
+            $urlPathKeyGenerator,
+            $snippetKeyGeneratorLocator,
+            $dataPoolReader,
+            $logger
+        );
+        $page = $pageBuilder->process();
+        $body = $page->getBody();
+
+        $this->assertEquals('<div>bar</div>', $body);
+    }
+}
