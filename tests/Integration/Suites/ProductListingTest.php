7edf0788d2f59ef22fb7f021778dfb4acb5e6880
Tim Bezhashvyly <tim.bezhashvyly@gmail.com>
2015-03-29 11:08:54 +0200

    Issue #184: Move writing product listing fixture into a key-value store into a separate method

--- tests/Integration/Suites/ProductListingTest.php
+++ tests/Integration/Suites/ProductListingTest.php
@@ -5,6 +5,7 @@ namespace Brera;
 use Brera\Context\VersionedContext;
 use Brera\DataPool\DataPoolReader;
 use Brera\DataPool\KeyValue\InMemory\InMemoryKeyValueStore;
+use Brera\DataPool\KeyValue\KeyValueStore;
 use Brera\DataPool\SearchEngine\InMemorySearchEngine;
 use Brera\Http\HttpUrl;
 use Brera\Product\ProductListingRequestHandler;
@@ -21,9 +22,7 @@ class ProductListingTest extends \PHPUnit_Framework_TestCase
 
         $dataPoolReader = new DataPoolReader($keyValueStore, $searchEngine);
 
-        $keyValueStore->set('_men-accessories_v:1_0', '{"source_id":"","root_snippet_code":"product_listing","page_snippet_codes":["product_in_listing_118235-251_language:de_DE_website:ru"]}');
-        $keyValueStore->set('product_listing__v:1.0', '<div>{{snippet product_1}}</div>');
-        $keyValueStore->set('product_in_listing_118235-251_language:de_DE_website:ru__v:1.0', 'bar');
+        $this->writeProductListingFixturesIntoKeyValueStore($keyValueStore);
 
         $url = HttpUrl::fromString('http://example.com/men-accessories');
         $context = new VersionedContext(DataVersion::fromVersionString('1.0'));
@@ -42,6 +41,27 @@ class ProductListingTest extends \PHPUnit_Framework_TestCase
         $page = $pageBuilder->process();
         $body = $page->getBody();
 
-        $this->assertEquals('<div>bar</div>', $body);
+        $expectedContentWithProductInListingPutIntoRootSnippet = '<div>bar</div>';
+
+        $this->assertEquals($expectedContentWithProductInListingPutIntoRootSnippet, $body);
+    }
+
+    private function writeProductListingFixturesIntoKeyValueStore(KeyValueStore $keyValueStore)
+    {
+        $productListingMetaDataSnippetKey = '_men-accessories_v:1_0';
+        $productListingMetaDataSnippetContent = '{
+            "source_id": "",
+            "root_snippet_code": "product_listing",
+            "page_snippet_codes": ["product_in_listing_118235-251_language:de_DE_website:ru"]
+        }';
+        $keyValueStore->set($productListingMetaDataSnippetKey, $productListingMetaDataSnippetContent);
+
+        $productListingRootSnippetKey = 'product_listing__v:1.0';
+        $productListingRootSnippetContent = '<div>{{snippet product_1}}</div>';
+        $keyValueStore->set($productListingRootSnippetKey, $productListingRootSnippetContent);
+
+        $productInListingSnippetKey = 'product_in_listing_118235-251_language:de_DE_website:ru__v:1.0';
+        $productInListingSnippetContent = 'bar';
+        $keyValueStore->set($productInListingSnippetKey, $productInListingSnippetContent);
     }
 }
