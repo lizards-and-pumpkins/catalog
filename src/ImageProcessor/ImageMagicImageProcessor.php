b212f5d7aee2de9e14858af82bb5e21f86a9a802
Fabian Blechschmidt <blechschmidt@fabian-blechschmidt.de>
2015-03-31 18:35:11 +0200

    Issue #67: Pass imagePath as second argument instead of own setter to exception

--- src/ImageProcessor/ImageMagicImageProcessor.php
+++ src/ImageProcessor/ImageMagicImageProcessor.php
@@ -26,9 +26,7 @@ class ImageMagicImageProcessor implements ImageProcessor
     public static function fromFile($imagePath)
     {
         if (!is_readable($imagePath)) {
-            $exception = new InvalidImageException("File \"$imagePath\" doesn't exist or is not readable.");
-            $exception->setImagePath($imagePath);
-            throw $exception;
+            throw new InvalidImageException("File \"$imagePath\" doesn't exist or is not readable.", $imagePath);
         }
 
         return new self($imagePath);
--- src/ImageProcessor/InvalidImageException.php
+++ src/ImageProcessor/InvalidImageException.php
@@ -4,24 +4,4 @@ namespace Brera\ImageProcessor;
 
 class InvalidImageException extends \Exception
 {
-    /**
-     * @var string
-     */
-    private $imagePath;
-
-    /**
-     * @return string
-     */
-    public function getImagePath()
-    {
-        return $this->imagePath;
-    }
-
-    /**
-     * @param string $imagePath
-     */
-    public function setImagePath($imagePath)
-    {
-        $this->imagePath = $imagePath;
-    }
 }
