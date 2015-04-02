32630a5e76945370f75014d52fb39dc7c239d25e
Tim Bezhashvyly <tim.bezhashvyly@gmail.com>
2015-04-03 00:15:21 +0200

    Issue #184: Refactor block tests

--- tests/Unit/Suites/Product/Block/ProductInListingBlockTest.php
+++ tests/Unit/Suites/Product/Block/ProductInListingBlockTest.php
@@ -15,32 +15,22 @@ use Brera\Renderer\Block;
  */
 class ProductInListingBlockTest extends \PHPUnit_Framework_TestCase
 {
-    use TestFileFixtureTrait;
-
     /**
      * @var Product|\PHPUnit_Framework_MockObject_MockObject
      */
     private $stubProduct;
 
     /**
-     * @var ProductInListingBlockRenderer|\PHPUnit_Framework_MockObject_MockObject
+     * @var ProductInListingBlock
      */
-    private $stubRenderer;
+    private $block;
 
     protected function setUp()
     {
-        $this->stubRenderer = $this->getMock(ProductInListingBlockRenderer::class, [], [], '', false);
+        $stubRenderer = $this->getMock(ProductInListingBlockRenderer::class, [], [], '', false);
         $this->stubProduct = $this->getMock(Product::class, [], [], '', false);
-    }
 
-    /**
-     * @return ProductInListingBlock
-     */
-    private function createInstance()
-    {
-        $template = 'dummy-template.phtml';
-        $blockName = 'test-name';
-        return new ProductInListingBlock($this->stubRenderer, $template, $blockName, $this->stubProduct);
+        $this->block = new ProductInListingBlock($stubRenderer, 'foo.phtml', 'foo', $this->stubProduct);
     }
 
     /**
@@ -48,41 +38,6 @@ class ProductInListingBlockTest extends \PHPUnit_Framework_TestCase
      */
     public function itShouldBeABlock()
     {
-        $this->assertInstanceOf(Block::class, $this->createInstance());
-    }
-
-    /**
-     * @test
-     */
-    public function itShouldReturnProductAttributeValue()
-    {
-        $attributeCode = 'name';
-        $attributeValue = 'foo';
-        $this->stubProduct->expects($this->once())
-            ->method('getAttributeValue')
-            ->with($attributeCode)
-            ->willReturn($attributeValue);
-
-        $block = $this->createInstance();
-
-        $this->assertEquals($attributeValue, $block->getProductAttributeValue($attributeCode));
-    }
-
-    /**
-     * @test
-     */
-    public function itShouldReturnEmptyStringIfAttributeIsNotFound()
-    {
-        $stubException = $this->getMock(ProductAttributeNotFoundException::class);
-
-        $attributeCode = 'bar';
-        $this->stubProduct->expects($this->once())
-            ->method('getAttributeValue')
-            ->with($attributeCode)
-            ->willThrowException($stubException);
-
-        $productInListingBlock = $this->createInstance();
-        $result = $productInListingBlock->getProductAttributeValue($attributeCode);
-        $this->assertSame('', $result);
+        $this->assertInstanceOf(Block::class, $this->block);
     }
 }
