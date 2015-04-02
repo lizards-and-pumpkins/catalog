1411d0b8468b9cae4a0bbfe9bfad1d2cfe9c7431
Fabian Blechschmidt <blechschmidt@fabian-blechschmidt.de>
2015-04-02 10:19:49 +0200

    Issue #58: cleanup after test

--- tests/Integration/Suites/ImageImportTest.php
+++ tests/Integration/Suites/ImageImportTest.php
@@ -29,6 +29,35 @@ class ImageImportTest extends \PHPUnit_Framework_TestCase
         $this->assertEmpty($factory->getLogger()->getMessages());
     }
 
+    protected function tearDown()
+    {
+        // TODO value is from commonfactory
+        $dir = sys_get_temp_dir() . '/brera';
+        $this->recursiveRemoveDirectory($dir);
+        mkdir($dir);
+    }
+
+    /**
+     * @param string $dir
+     */
+    protected function recursiveRemoveDirectory($dir)
+    {
+        if (is_dir($dir)) {
+            $objects = scandir($dir);
+            foreach ($objects as $object) {
+                if ($object != "." && $object != "..") {
+                    if (filetype($dir . "/" . $object) == "dir") {
+                        $this->recursiveRemoveDirectory($dir . "/" . $object);
+                    } else {
+                        unlink($dir . "/" . $object);
+                    }
+                }
+            }
+            reset($objects);
+            rmdir($dir);
+        }
+    }
+
     /**
      * @return PoCMasterFactory
      */
