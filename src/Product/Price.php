4c1fac3a8e68827d5608dd4797f6754f81c7cc90
Tim Bezhashvyly <tim.bezhashvyly@gmail.com>
2015-04-11 11:37:44 +0200

    Issue #195: Implement price value object

---
+++ src/Product/Price.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Brera\Product;
+
+class Price implements Money
+{
+    const NUM_DECIMAL_POINTS = 2;
+
+    /**
+     * @var int
+     */
+    private $amount;
+
+    /**
+     * @param int $amount
+     */
+    public function __construct($amount)
+    {
+        if (!is_int($amount)) {
+            throw new InvalidArgumentException(sprintf('Can not create a price from %s', gettype($amount)));
+        }
+
+        $this->amount = $amount;
+    }
+
+    /**
+     * @param string $amountString
+     * @return Price
+     */
+    public static function fromString($amountString)
+    {
+        if (!is_string($amountString)) {
+            throw new InvalidArgumentException(sprintf('Can not create a price from %s', gettype($amountString)));
+        }
+
+        $amountInt = (int) round($amountString, self::NUM_DECIMAL_POINTS) * pow(10, self::NUM_DECIMAL_POINTS);
+
+        return new static($amountInt);
+    }
+    
+    /**
+     * @return int
+     */
+    public function getAmount()
+    {
+        return $this->amount;
+    }
+}
