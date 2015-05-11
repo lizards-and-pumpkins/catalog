225667feee727447c7c885297e1c02abb528d51a
Tim Bezhashvyly <tim.bezhashvyly@gmail.com>
2015-05-12 01:04:22 +0200

    Issue #67: Fix styling issue of LocalFilesystem

--- src/Utils/LocalFilesystem.php
+++ src/Utils/LocalFilesystem.php
@@ -4,11 +4,14 @@ namespace Brera\Utils;
 
 class LocalFilesystem
 {
+    /**
+     * @param string $directoryPath
+     */
     public function removeDirectoryAndItsContent($directoryPath)
     {
         $directoryIterator = new \RecursiveDirectoryIterator($directoryPath, \FilesystemIterator::SKIP_DOTS);
 
-        foreach(new \RecursiveIteratorIterator($directoryIterator, \RecursiveIteratorIterator::CHILD_FIRST) as $path) {
+        foreach (new \RecursiveIteratorIterator($directoryIterator, \RecursiveIteratorIterator::CHILD_FIRST) as $path) {
             $path->isDir() && !$path->isLink() ? rmdir($path->getPathname()) : unlink($path->getPathname());
         }
 
