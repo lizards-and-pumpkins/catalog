4c8ec20b24e74b2393f0e064b629f28c569715b8
Tim Bezhashvyly <tim.bezhashvyly@gmail.com>
2015-05-19 01:26:34 +0200

    Issue #67: Introduce ImageProcessorCollection

---
+++ tests/Unit/Suites/Image/ImageProcessorCollectionTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Brera\Image;
+
+/**
+ * @covers \Brera\Image\ImageProcessorCollection
+ */
+class ImageProcessorCollectionTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function itShouldProcessAllProcessorsOfACollection()
+    {
+        $processor1 = $this->getMock(ImageProcessor::class, [], [], '', false);
+        $processor1->expects($this->once())
+            ->method('process');
+        $processor2 = $this->getMock(ImageProcessor::class, [], [], '', false);
+        $processor2->expects($this->once())
+            ->method('process');
+
+        $collection = new ImageProcessorCollection();
+        $collection->add($processor1);
+        $collection->add($processor2);
+
+        $collection->process('imageFileName');
+    }
+}
