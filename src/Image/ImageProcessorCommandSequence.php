2276a43edb392066e5b976c8ecdae13058fec017
Tim Bezhashvyly <tim.bezhashvyly@gmail.com>
2015-05-24 10:41:12 +0200

    Issue #67: Replace array_push with direct array access

--- src/Image/ImageProcessorCommandSequence.php
+++ src/Image/ImageProcessorCommandSequence.php
@@ -11,7 +11,7 @@ class ImageProcessorCommandSequence
 
     public function addCommand(ImageProcessorCommand $command)
     {
-        array_push($this->commands, $command);
+        $this->commands[] = $command;
     }
 
     /**
