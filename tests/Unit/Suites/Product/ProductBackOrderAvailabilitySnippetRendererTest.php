974e1abe0c57eb658ec42a86cbe5d1d14eb13aea
Tim Bezhashvyly <tim.bezhashvyly@gmail.com>
2015-07-12 10:40:09 +0200

    Issue #246: Implement ProductBackOrdersAvailabilitySnippetRenderer

---
+++ tests/Unit/Suites/Product/ProductBackOrderAvailabilitySnippetRendererTest.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace Brera\Product;
+
+use Brera\Context\Context;
+use Brera\Context\ContextSource;
+use Brera\Snippet;
+use Brera\SnippetKeyGenerator;
+use Brera\SnippetList;
+use Brera\SnippetRenderer;
+
+/**
+ * @covers \Brera\Product\ProductBackOrderAvailabilitySnippetRenderer
+ */
+class ProductBackOrderAvailabilitySnippetRendererTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var ProductBackOrderAvailabilitySnippetRenderer
+     */
+    private $renderer;
+
+    /**
+     * @var SnippetList|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private $mockSnippetList;
+
+    /**
+     * @var SnippetKeyGenerator|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private $mockSnippetKeyGenerator;
+
+    /**
+     * @var string
+     */
+    private $dummyBackOrderAvialabilityAttributeCode = 'foo';
+
+    protected function setUp()
+    {
+        $this->mockSnippetList = $this->getMock(SnippetList::class);
+        $this->mockSnippetKeyGenerator = $this->getMock(SnippetKeyGenerator::class);
+
+        $this->renderer = new ProductBackOrderAvailabilitySnippetRenderer(
+            $this->mockSnippetList,
+            $this->mockSnippetKeyGenerator,
+            $this->dummyBackOrderAvialabilityAttributeCode
+        );
+    }
+
+    public function testSnippetRendererInterfaceIsImplemented()
+    {
+        $this->assertInstanceOf(SnippetRenderer::class, $this->renderer);
+    }
+
+    public function testEmptySnippetListIsReturned()
+    {
+        $mockProductSource = $this->getMock(ProductSource::class, [], [], '', false);
+
+        $mockContextSource = $this->getMock(ContextSource::class, [], [], '', false);
+        $mockContextSource->method('getAllAvailableContexts')
+            ->willReturn([]);
+
+        $result = $this->renderer->render($mockProductSource, $mockContextSource);
+
+        $this->assertInstanceOf(SnippetList::class, $result);
+        $this->assertEmpty($result);
+    }
+
+    public function testSnippetListContainingSnippetWithGivenKeyAndBackOrderAvailabilityIsReturned()
+    {
+        $stubContext = $this->getMock(Context::class);
+        $dummyBackOrderAvailabilitySnippetKey = 'bar';
+        $dummyBackOrderAvailabilityAttributeValue = '1';
+
+        $mockProduct = $this->getMock(Product::class, [], [], '', false);
+        $mockProduct->method('getAttributeValue')
+            ->with($this->dummyBackOrderAvialabilityAttributeCode)
+            ->willReturn($dummyBackOrderAvailabilityAttributeValue);
+
+        $mockProductSource = $this->getMock(ProductSource::class, [], [], '', false);
+        $mockProductSource->method('getProductForContext')
+            ->willReturn($mockProduct);
+
+        $mockContextSource = $this->getMock(ContextSource::class, [], [], '', false);
+        $mockContextSource->method('getAllAvailableContexts')
+            ->willReturn([$stubContext]);
+
+        $this->mockSnippetKeyGenerator->method('getKeyForContext')
+            ->willReturn($dummyBackOrderAvailabilitySnippetKey);
+
+        $expectedSnippet = Snippet::create(
+            $dummyBackOrderAvailabilitySnippetKey,
+            $dummyBackOrderAvailabilityAttributeValue
+        );
+
+        $this->mockSnippetList->expects($this->once())
+            ->method('add')
+            ->with($expectedSnippet);
+
+        $this->renderer->render($mockProductSource, $mockContextSource);
+    }
+}
