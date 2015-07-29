a0d16d03e9726baaf59526b8bcc135de3f60f790
Tim Bezhashvyly <tim.bezhashvyly@gmail.com>
2015-07-29 07:37:45 +0200

    Issue #258: Normalize UrlKey upon creation

--- tests/Unit/Suites/UrlKeyTest.php
+++ tests/Unit/Suites/UrlKeyTest.php
@@ -20,4 +20,31 @@ class UrlKeyTest extends \PHPUnit_Framework_TestCase
 
         $this->assertEquals($dummyKey, (string) $urlKey);
     }
+
+    /**
+     * @dataProvider urlKeySourceProvider
+     * @param string $urlKeySource
+     * @param string $expectedUrlKey
+     */
+    public function testDisallowedCharactersAreReplacedWithUnderscores($urlKeySource, $expectedUrlKey)
+    {
+        $urlKey = UrlKey::fromString($urlKeySource);
+        $this->assertEquals($expectedUrlKey, (string) $urlKey);
+    }
+
+    /**
+     * @return array[]
+     */
+    public function urlKeySourceProvider()
+    {
+        return [
+            ['foo', 'foo'],
+            ['foo_:bar', 'foo_:bar'],
+            ['foo1/bar', 'foo1_bar'],
+            ['bar.html', 'bar_html'],
+            ['/foo%', '_foo_'],
+            ['///', '___'],
+            ['$&"#', '____'],
+        ];
+    }
 }
