683d230c1e3e7ce0e8e61eb69117e8c7b4147bc6
Vinai Kopp <vinai@netzarbeiter.com>
2015-08-28 16:04:20 +0200

    Issue #311: Implement locale context determination based on request

--- src/Context/LocaleContextDecorator.php
+++ src/Context/LocaleContextDecorator.php
@@ -3,15 +3,104 @@
 
 namespace Brera\Context;
 
+use Brera\Http\HttpRequest;
+
 class LocaleContextDecorator extends ContextDecorator
 {
     const CODE = 'locale';
-    
+
+    private $defaultLocale = 'de_DE';
+
+    private $languageToLocaleMap = [
+        'de' => 'de_DE',
+        'en' => 'en_US'
+    ];
+
     /**
      * @return string
      */
     protected function getCode()
     {
         return self::CODE;
+    }
+
+    /**
+     * @return string
+     */
+    protected function getValueFromContext()
+    {
+        if ($this->isLocalePresentInSourceData()) {
+            return $this->getLocaleFromSourceData();
+        }
+        if ($this->isRequestPresentInSourceData()) {
+            return $this->getLocaleFromRequest();
+        }
+        throw new UnableToDetermineLocaleException(sprintf(
+            'Unable to determine locale from context source data ("%s" and "request" not present)',
+            $this->getCode()
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    private function getLocaleFromRequest()
+    {
+        $locale = $this->getLocaleFromRequestPath();
+        return isset($this->languageToLocaleMap[$locale]) ?
+            $this->languageToLocaleMap[$locale] :
+            $this->defaultLocale;
+    }
+
+    /**
+     * @return string
+     */
+    private function getLocaleFromRequestPath()
+    {
+        $firstPathPart = $this->getFirstRequestPathPart();
+        return substr($firstPathPart, strpos($firstPathPart, '_') + 1);
+    }
+
+    /**
+     * @return string
+     */
+    private function getFirstRequestPathPart()
+    {
+        $path = $this->getRequest()->getUrl()->getPathRelativeToWebFront();
+        return '' !== $path ?
+            explode('/', $path)[0] :
+            '';
+    }
+
+    /**
+     * @return HttpRequest
+     */
+    private function getRequest()
+    {
+        return $this->getSourceData()['request'];
+    }
+
+    /**
+     * @return bool
+     */
+    private function isLocalePresentInSourceData()
+    {
+        return isset($this->getSourceData()['locale']);
+    }
+
+    /**
+     * @return mixed
+     */
+    private function getLocaleFromSourceData()
+    {
+        return $this->getSourceData()['locale'];
+    }
+
+    /**
+     * @return bool
+     */
+    private function isRequestPresentInSourceData()
+    {
+        return isset($this->getSourceData()['request']);
     }
 }
