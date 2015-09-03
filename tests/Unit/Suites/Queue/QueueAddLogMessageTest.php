9755c40ee9aac4b757e545d386b4d1beddcb134d
Vinai Kopp <vinai@netzarbeiter.com>
2015-09-03 22:34:25 +0200

    Issue #342: Implement logging queue decorator

---
+++ tests/Unit/Suites/Queue/QueueAddLogMessageTest.php
@@ -0,0 +1,63 @@
+<?php
+
+
+namespace Brera\Queue;
+
+/**
+ * @covers Brera\Queue\QueueAddLogMessage
+ */
+class QueueAddLogMessageTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Queue|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private $stubQueue;
+
+    protected function setUp()
+    {
+        $this->stubQueue = $this->getMock(Queue::class);
+    }
+    public function testItUsesTheClassNameForTheStringRepresentationForObjects()
+    {
+        $expected = sprintf('%s instance added to queue', __CLASS__);
+        $this->assertSame($expected, (string) new QueueAddLogMessage($this, $this->stubQueue));
+    }
+
+    /**
+     * @param array|int|float|string|resource|null $nonObject
+     * @dataProvider nonObjectDataProvider
+     */
+    public function testItUsesTheDataTypeForTheStringRepresentationForNonObjects($nonObject, $expected)
+    {
+        $this->assertSame($expected, (string) new QueueAddLogMessage($nonObject, $this->stubQueue));
+    }
+
+    /**
+     * @return array[]
+     */
+    public function nonObjectDataProvider()
+    {
+        return [
+            [[], 'Array added to queue'],
+            [2, 'Integer added to queue'],
+            [0.9, 'Double added to queue'],
+            ['foo', 'String added to queue'],
+            [fopen(__FILE__, 'r'), 'Resource added to queue'],
+            [null, 'NULL added to queue']
+        ];
+    }
+
+    public function testTheQueueIsPartOfTheMessageContext()
+    {
+        $logMessage = new QueueAddLogMessage(new \stdClass(), $this->stubQueue);
+        $this->assertArrayHasKey('queue', $logMessage->getContext());
+        $this->assertSame($this->stubQueue, $logMessage->getContext()['queue']);
+    }
+
+    public function testTheAddedDataIsPartOfTheMessageContext()
+    {
+        $logMessage = new QueueAddLogMessage($this, $this->stubQueue);
+        $this->assertArrayHasKey('data', $logMessage->getContext());
+        $this->assertSame($this, $logMessage->getContext()['data']);
+    }
+}
