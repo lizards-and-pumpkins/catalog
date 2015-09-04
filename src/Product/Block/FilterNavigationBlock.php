5aa1e4920c3aea8d3663343099894b7da9265b12
Tim Bezhashvyly <tim.bezhashvyly@gmail.com>
2015-09-04 12:48:50 +0200

    Issue #230: Implement building of filter navigation query string

--- src/Product/Block/FilterNavigationBlock.php
+++ src/Product/Block/FilterNavigationBlock.php
@@ -8,6 +8,13 @@ use Brera\Renderer\InvalidDataObjectException;
 
 class FilterNavigationBlock extends Block
 {
+    const VALUES_SEPARATOR = ',';
+
+    /**
+     * @var array[]
+     */
+    private $lazyLoadedSelectedFilters;
+
     /**
      * @return FilterNavigationFilterCollection
      */
@@ -38,5 +45,37 @@ class FilterNavigationBlock extends Block
     private function getVariableType($variable)
     {
         return 'object' !== gettype($variable) ? gettype($variable) : get_class($variable);
+    }
+
+    /**
+     * @param string $filterCode
+     * @param string $filterValue
+     * @return string
+     */
+    public function getQueryString($filterCode, $filterValue)
+    {
+        $selectedFilters = $this->getSelectedFilters();
+        foreach ($selectedFilters as $selectedFilterCode => $selectedValues) {
+            if ($filterCode === $selectedFilterCode) {
+                if (in_array($filterValue, $selectedValues)) {
+                    $selectedValues = array_diff($selectedValues, [$filterValue]);
+                } else {
+                    $selectedValues[] = $filterValue;
+                }
+            }
+            $selectedFilters[$selectedFilterCode] = implode(self::VALUES_SEPARATOR, $selectedValues);
+        }
+
+        /* TODO: Replace http_build_query w/ HttpUrl method */
+        return http_build_query(array_filter($selectedFilters));
+    }
+
+    private function getSelectedFilters()
+    {
+        if (null === $this->lazyLoadedSelectedFilters) {
+            $this->lazyLoadedSelectedFilters = $this->getFilterCollection()->getSelectedFilters();
+        }
+
+        return $this->lazyLoadedSelectedFilters;
     }
 }
--- tests/Unit/Suites/Product/Block/FilterNavigationBlockTest.php
+++ tests/Unit/Suites/Product/Block/FilterNavigationBlockTest.php
@@ -23,10 +23,17 @@ class FilterNavigationBlockTest extends \PHPUnit_Framework_TestCase
      */
     private $stubBlockRenderer;
 
+    /**
+     * @var FilterNavigationFilterCollection|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private $stubFilterCollection;
+
     protected function setUp()
     {
         $this->stubBlockRenderer = $this->getMock(BlockRenderer::class, [], [], '', false);
-        $stubDataObject = $this->getMock(FilterNavigationFilterCollection::class, [], [], '', false);
+        $this->stubFilterCollection = $this->getMock(FilterNavigationFilterCollection::class, [], [], '', false);
+        $stubDataObject = $this->stubFilterCollection;
+
         $this->block = new FilterNavigationBlock($this->stubBlockRenderer, 'foo.phtml', 'foo', $stubDataObject);
     }
 
@@ -50,5 +57,105 @@ class FilterNavigationBlockTest extends \PHPUnit_Framework_TestCase
     {
         $result = $this->block->getFilterCollection();
         $this->assertInstanceOf(FilterNavigationFilterCollection::class, $result);
+    }
+
+    public function testQueryStringContainsOnlyGivenFilterIfNoFiltersAreCurrentlySelected()
+    {
+        $filterCode = 'foo';
+        $filterValue = 'bar';
+
+        $this->stubFilterCollection->method('getSelectedFilters')->willReturn([$filterCode => []]);
+
+        $result = $this->block->getQueryString($filterCode, $filterValue);
+        $expectedQueryString = sprintf('%s=%s', $filterCode, $filterValue);
+
+        $this->assertEquals($expectedQueryString, $result);
+    }
+    
+    public function testQueryStringContainsAllSelectedFiltersPlusGivenFilter()
+    {
+        $newFilterCode = 'foo';
+        $newFilterValue = 'bar';
+
+        $existingFilterCode = 'baz';
+        $existingFilterSelectedValue = 'qux';
+
+        $this->stubFilterCollection->method('getSelectedFilters')
+            ->willReturn([$existingFilterCode => [$existingFilterSelectedValue], $newFilterCode => []]);
+
+        $result = $this->block->getQueryString($newFilterCode, $newFilterValue);
+        $resultTokensArray = explode('&', $result);
+
+        $expectedOldToken = sprintf('%s=%s', $existingFilterCode, $existingFilterSelectedValue);
+        $expectedNewToken = sprintf('%s=%s', $newFilterCode, $newFilterValue);
+
+        $this->assertCount(2, $resultTokensArray);
+        $this->assertContains($expectedOldToken, $resultTokensArray);
+        $this->assertContains($expectedNewToken, $resultTokensArray);
+    }
+
+    public function testSelectedValueShouldBeAddedToPreviouslySelectedValuesOfAFilter()
+    {
+        $filterCode = 'foo';
+        $filterValue = 'bar';
+        $filterPreviouslySelectedValue = 'baz';
+
+        $this->stubFilterCollection->method('getSelectedFilters')
+            ->willReturn([$filterCode => [$filterPreviouslySelectedValue]]);
+
+        $result = $this->block->getQueryString($filterCode, $filterValue);
+        $expectedQueryString = sprintf(
+            '%s=%s%s%s',
+            $filterCode,
+            $filterPreviouslySelectedValue,
+            urlencode(FilterNavigationBlock::VALUES_SEPARATOR),
+            $filterValue
+        );
+
+        $this->assertSame($expectedQueryString, $result);
+    }
+    
+    public function testFilterValueIsRemovedFromQueryStringIfPreviouslySelected()
+    {
+        $filterCode = 'foo';
+        $filterValue = 'bar';
+        $filterOtherSelectedValue = 'some-other-value-which-should-not-be-removed';
+
+        $otherFilterCode = 'baz';
+        $otherFilterSelectedValue = 'qux';
+
+        $this->stubFilterCollection->method('getSelectedFilters')->willReturn([
+            $otherFilterCode => [$otherFilterSelectedValue],
+            $filterCode      => [$filterValue, $filterOtherSelectedValue]
+        ]);
+
+        $result = $this->block->getQueryString($filterCode, $filterValue);
+        $resultTokensArray = explode('&', $result);
+
+        $expectedFilterToken = sprintf('%s=%s', $filterCode, $filterOtherSelectedValue);
+        $expectedOtherFilterToken = sprintf('%s=%s', $otherFilterCode, $otherFilterSelectedValue);
+
+        $this->assertCount(2, $resultTokensArray);
+        $this->assertContains($expectedOtherFilterToken, $resultTokensArray);
+        $this->assertContains($expectedFilterToken, $resultTokensArray);
+    }
+
+    public function testFilterIsRemovedFromQueryStringIfLastSelectedValueWasUnset()
+    {
+        $filterCode = 'foo';
+        $filterValue = 'bar';
+
+        $otherFilterCode = 'baz';
+        $otherFilterSelectedValue = 'qux';
+
+        $this->stubFilterCollection->method('getSelectedFilters')->willReturn([
+            $otherFilterCode => [$otherFilterSelectedValue],
+            $filterCode      => [$filterValue]
+        ]);
+
+        $result = $this->block->getQueryString($filterCode, $filterValue);
+        $expectedQueryString = sprintf('%s=%s', $otherFilterCode, $otherFilterSelectedValue);
+
+        $this->assertSame($expectedQueryString, $result);
     }
 }
