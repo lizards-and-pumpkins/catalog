4246701d64834f044fc047547208e4eefa08bbc4
Vinai Kopp <vinai@netzarbeiter.com>
2015-09-11 17:20:51 +0200

    Issue #345: Only keep 500 log messages in memory

--- tests/Unit/Suites/Log/InMemoryLoggerTest.php
+++ tests/Unit/Suites/Log/InMemoryLoggerTest.php
@@ -7,15 +7,51 @@ namespace Brera\Log;
  */
 class InMemoryLoggerTest extends \PHPUnit_Framework_TestCase
 {
-    public function testMessageIsStoredInMemory()
+    /**
+     * @var InMemoryLogger
+     */
+    private $logger;
+
+    protected function setUp()
     {
-        $stubLogMessage = $this->getMock(LogMessage::class);
+        $this->logger = new InMemoryLogger();
+    }
 
-        $logger = new InMemoryLogger();
-        $logger->log($stubLogMessage);
+    public function testItStoresTheMessagesInOrder()
+    {
+        $stubLogMessage1 = $this->getMock(LogMessage::class);
+        $stubLogMessage2 = $this->getMock(LogMessage::class);
+        $stubLogMessage3 = $this->getMock(LogMessage::class);
+        
+        $this->logger->log($stubLogMessage1);
+        $this->logger->log($stubLogMessage2);
+        $this->logger->log($stubLogMessage3);
+        
+        $this->assertSame([
+            $stubLogMessage1,
+            $stubLogMessage2,
+            $stubLogMessage3
+        ], $this->logger->getMessages());
+    }
 
-        $messages = $logger->getMessages();
 
-        $this->assertContains($stubLogMessage, $messages);
+    public function testItOnlyKeepsA500MessagesRollingWindow()
+    {
+        $stubLogMessage1 = $this->getMock(LogMessage::class);
+        $stubLogMessage2 = $this->getMock(LogMessage::class);
+        $otherLogMessage = $this->getMock(LogMessage::class);
+
+        $this->logger->log($stubLogMessage1);
+        $this->logger->log($stubLogMessage2);
+
+        for ($i = 0; $i < 498; $i++) {
+            $this->logger->log($otherLogMessage);
+        }
+        $this->assertSame($stubLogMessage1, $this->logger->getMessages()[0]);
+        $this->assertSame($stubLogMessage2, $this->logger->getMessages()[1]);
+        
+        $this->logger->log($otherLogMessage);
+        $this->assertSame($stubLogMessage2, $this->logger->getMessages()[0]);
+        $this->assertSame($otherLogMessage, $this->logger->getMessages()[1]);
     }
 }
