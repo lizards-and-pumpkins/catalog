6c52cf939bfbf80cd46a3b73c4e9d888049ec4cc
Vinai Kopp <vinai@netzarbeiter.com>
2015-09-12 14:17:01 +0200

    Issue #356: Remove extracted LoggingDomainEventHandlerFactory from script

--- bin/eventConsumer.php
+++ bin/eventConsumer.php
@@ -3,115 +3,14 @@
 
 namespace Brera;
 
-use Brera\Content\ContentBlockWasUpdatedDomainEvent;
-use Brera\Content\ContentBlockWasUpdatedDomainEventHandler;
-use Brera\Image\ImageWasUpdatedDomainEvent;
-use Brera\Image\ImageWasUpdatedDomainEventHandler;
-use Brera\Product\ProductListingWasUpdatedDomainEvent;
-use Brera\Product\ProductListingWasUpdatedDomainEventHandler;
-use Brera\Product\ProductStockQuantityWasUpdatedDomainEvent;
-use Brera\Product\ProductStockQuantityWasUpdatedDomainEventHandler;
-use Brera\Product\ProductWasUpdatedDomainEvent;
-use Brera\Product\ProductWasUpdatedDomainEventHandler;
+use Brera\Projection\LoggingDomainEventHandlerFactory;
 
 require __DIR__ . '/../vendor/autoload.php';
-
-class LoggingEventHandlerFactory implements Factory, DomainEventFactory
-{
-    use FactoryTrait;
-
-    /**
-     * @var CommonFactory
-     */
-    private $commonFactory;
-
-    private function getCommonFactory()
-    {
-        if (null === $this->commonFactory) {
-            $this->commonFactory = new CommonFactory();
-            $this->commonFactory->setMasterFactory($this->getMasterFactory());
-        }
-        return $this->commonFactory;
-    }
-    
-    /**
-     * @param ProductWasUpdatedDomainEvent $event
-     * @return ProductWasUpdatedDomainEventHandler
-     */
-    public function createProductWasUpdatedDomainEventHandler(ProductWasUpdatedDomainEvent $event)
-    {
-        $commonFactory = $this->getCommonFactory();
-        return $commonFactory->createProcessTimeLoggingDomainEventDecorator(
-            $commonFactory->createProductWasUpdatedDomainEventHandler($event)
-        );
-    }
-
-    /**
-     * @param TemplateWasUpdatedDomainEvent $event
-     * @return TemplateWasUpdatedDomainEventHandler
-     */
-    public function createTemplateWasUpdatedDomainEventHandler(TemplateWasUpdatedDomainEvent $event)
-    {
-        $commonFactory = $this->getCommonFactory();
-        return $commonFactory->createProcessTimeLoggingDomainEventDecorator(
-            $commonFactory->createTemplateWasUpdatedDomainEventHandler($event)
-        );
-    }
-
-    /**
-     * @param ImageWasUpdatedDomainEvent $event
-     * @return ImageWasUpdatedDomainEventHandler
-     */
-    public function createImageWasUpdatedDomainEventHandler(ImageWasUpdatedDomainEvent $event)
-    {
-        $commonFactory = $this->getCommonFactory();
-        return $commonFactory->createProcessTimeLoggingDomainEventDecorator(
-            $commonFactory->createImageWasUpdatedDomainEventHandler($event)
-        );
-    }
-
-    /**
-     * @param ProductListingWasUpdatedDomainEvent $event
-     * @return ProductListingWasUpdatedDomainEventHandler
-     */
-    public function createProductListingWasUpdatedDomainEventHandler(ProductListingWasUpdatedDomainEvent $event)
-    {
-        $commonFactory = $this->getCommonFactory();
-        return $commonFactory->createProcessTimeLoggingDomainEventDecorator(
-            $commonFactory->createProductListingWasUpdatedDomainEventHandler($event)
-        );
-    }
-
-    /**
-     * @param ProductStockQuantityWasUpdatedDomainEvent $event
-     * @return ProductStockQuantityWasUpdatedDomainEventHandler
-     */
-    public function createProductStockQuantityWasUpdatedDomainEventHandler(
-        ProductStockQuantityWasUpdatedDomainEvent $event
-    ) {
-        $commonFactory = $this->getCommonFactory();
-        return $commonFactory->createProcessTimeLoggingDomainEventDecorator(
-            $commonFactory->createProductStockQuantityWasUpdatedDomainEventHandler($event)
-        );
-    }
-
-    /**
-     * @param ContentBlockWasUpdatedDomainEvent $event
-     * @return ContentBlockWasUpdatedDomainEventHandler
-     */
-    public function createContentBlockWasUpdatedDomainEventHandler(ContentBlockWasUpdatedDomainEvent $event)
-    {
-        $commonFactory = $this->getCommonFactory();
-        return $commonFactory->createProcessTimeLoggingDomainEventDecorator(
-            $commonFactory->createContentBlockWasUpdatedDomainEventHandler($event)
-        );
-    }
-}
 
 $factory = new SampleMasterFactory();
 $factory->register(new CommonFactory());
 $factory->register(new SampleFactory());
-$factory->register(new LoggingEventHandlerFactory());
+$factory->register(new LoggingDomainEventHandlerFactory());
 
 $eventConsumer = $factory->createDomainEventConsumer();
 $eventConsumer->process();
