5b8d54835023c136190372511ba5135720f75b4b
Vinai Kopp <vinai@netzarbeiter.com>
2015-09-12 07:47:00 +0200

    Issue #356: Add logging queue decorator to event consumer script

--- bin/eventConsumer.php
+++ bin/eventConsumer.php
@@ -3,11 +3,49 @@
 
 namespace Brera;
 
+use Brera\Log\Writer\FileLogMessageWriter;
+use Brera\Log\Writer\LogMessageWriter;
+use Brera\Queue\File\FileQueue;
+use Brera\Queue\LoggingQueueDecorator;
+use Brera\Queue\Queue;
+
 require __DIR__ . '/../vendor/autoload.php';
 
 $factory = new SampleMasterFactory();
 $factory->register(new CommonFactory());
 $factory->register(new SampleFactory());
+
+
+class LoggingQueueFactory implements Factory
+{
+    use FactoryTrait;
+
+    /**
+     * @return Queue
+     */
+    public function createEventQueue()
+    {
+        $storagePath = sys_get_temp_dir() . '/brera/event-queue/content';
+        $lockFile = sys_get_temp_dir() . '/brera/event-queue/lock';
+        return new LoggingQueueDecorator(
+            new FileQueue($storagePath, $lockFile),
+            $this->getMasterFactory()->getLogger()
+        );
+    }
+
+    /**
+     * @return Queue
+     */
+    public function createCommandQueue()
+    {
+        $storagePath = sys_get_temp_dir() . '/brera/command-queue/content';
+        $lockFile = sys_get_temp_dir() . '/brera/command-queue/lock';
+        return new LoggingQueueDecorator(
+            new FileQueue($storagePath, $lockFile),
+            $this->getMasterFactory()->getLogger()
+        );
+    }
+}
 
 $eventConsumer = $factory->createDomainEventConsumer();
 $eventConsumer->process();
