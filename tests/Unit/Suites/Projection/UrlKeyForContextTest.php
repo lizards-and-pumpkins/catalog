e12329ddf76a999ad2fb53b8b42574e78843298d
Vinai Kopp <vinai@netzarbeiter.com>
2015-09-16 11:57:39 +0200

    Issue #312: Add getContextAsString method to UrlKeyForContext

--- tests/Unit/Suites/Projection/UrlKeyForContextTest.php
+++ tests/Unit/Suites/Projection/UrlKeyForContextTest.php
@@ -54,4 +54,10 @@ class UrlKeyForContextTest extends \PHPUnit_Framework_TestCase
         $this->stubContext->expects($this->once())->method('getValue')->with('test')->willReturn('result');
         $this->assertSame('result', $this->urlKeyForContext->getContextValue('test'));
     }
+
+    public function testItDelegatesToTheContextWhenGettingTheContextAsAString()
+    {
+        $this->stubContext->expects($this->once())->method('toString')->willReturn('result');
+        $this->assertSame('result', $this->urlKeyForContext->getContextAsString());
+    }
 }
