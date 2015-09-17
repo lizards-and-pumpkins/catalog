ff28c11fc74240766f74c3d144a8307deac4b56f
Vinai Kopp <vinai@netzarbeiter.com>
2015-09-17 21:21:31 +0100

    Issue #354: Make clearFileStorages implement BaseCliCommand

--- bin/clearFileStorages.php
+++ bin/clearFileStorages.php
@@ -3,15 +3,45 @@
 
 namespace LizardsAndPumpkins;
 
+use League\CLImate\CLImate;
+use LizardsAndPumpkins\Utils\BaseCliCommand;
+
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$factory = new SampleMasterFactory();
-$factory->register(new CommonFactory());
-$factory->register(new SampleFactory());
+class ClearFileStorage extends BaseCliCommand
+{
+    /**
+     * @var MasterFactory
+     */
+    private $factory;
 
-$factory->createDataPoolWriter()->clear();
-$factory->createCommandQueue()->clear();
-$factory->createEventQueue()->clear();
+    private function __construct(MasterFactory $factory, CLImate $climate)
+    {
+        $this->factory = $factory;
+        $this->setCLImate($climate);
+    }
 
-printf("Cleared data pool and queues\n");
-printf("Storage dir: %s\n", sys_get_temp_dir() . '/lizards-and-pumpkins');
+    /**
+     * @return ClearFileStorage
+     */
+    public static function bootstrap()
+    {
+        $factory = new SampleMasterFactory();
+        $factory->register(new CommonFactory());
+        $factory->register(new SampleFactory());
+        
+        return new self($factory, new CLImate());
+    }
+    
+    protected function execute(CLImate $climate)
+    {
+        $this->factory->createDataPoolWriter()->clear();
+        $this->factory->createCommandQueue()->clear();
+        $this->factory->createEventQueue()->clear();
+
+        $this->output('Cleared data pool and queues');
+        $this->output(sprintf("Storage dir: %s\n", $this->factory->getFileStorageBasePathConfig()));
+    }
+}
+
+ClearFileStorage::bootstrap()->run();
