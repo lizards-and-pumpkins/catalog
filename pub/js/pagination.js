bcd598c1053b8ee745f41b9118549e14fa2efe76
Tim Bezhashvyly <tim.bezhashvyly@gmail.com>
2015-09-23 21:00:48 +0200

    Issue #402: Replace jQuery usage in pagination.js w/ pure JS

--- pub/js/pagination.js
+++ pub/js/pagination.js
@@ -1,60 +1,70 @@
-define(['url', 'jquery'], function (url, $) {
+define(['url'], function (url) {
+    var paginationQueryParameterName = 'p',
+        maxPaginationLinksToShow = 7;
+
+    var createPreviousPaginationItem = function () {
+        var item = document.createElement('LI'),
+            link = document.createElement('A');
+        link.className = 'prev';
+        link.href = url.updateQueryParameter(paginationQueryParameterName, 1);
+        link.innerHTML = '&#9664;';
+        item.appendChild(link);
+
+        return item;
+    };
+
+    var createNextPaginationItem = function (lastPageNumber) {
+        var item = document.createElement('LI'),
+            link = document.createElement('A');
+        link.className = 'next';
+        link.href = url.updateQueryParameter(paginationQueryParameterName, lastPageNumber);
+        link.innerHTML = '&#9654;';
+        item.appendChild(link);
+
+        return item;
+    };
+
     return {
         generatePagination: function (totalPageCount, paginationPlaceholderSelector) {
-            var paginationPlaceholder = $(paginationPlaceholderSelector);
+            var paginationPlaceholder = document.querySelector(paginationPlaceholderSelector);
 
-            if (!paginationPlaceholderSelector.length) {
+            if (null === paginationPlaceholderSelector) {
                 return;
             }
 
-            var maxPaginationLinksToShow = 7,
-                paginationQueryParameterName = 'p',
-                pagination = $('<ol/>'),
+            var pagination = document.createElement('OL'),
                 currentPageNumber = Math.max(1, url.getQueryParameterValue(paginationQueryParameterName));
 
             if (totalPageCount && 1 < currentPageNumber) {
-                pagination.append(
-                    $('<li/>').append(
-                        $('<a/>').addClass('prev')
-                            .prop('href', url.updateQueryParameter(paginationQueryParameterName, 1)).html('&#9664;')
-                    )
-                );
+                pagination.appendChild(createPreviousPaginationItem());
             }
 
             for (var pageNumber = 1; pageNumber <= totalPageCount; pageNumber++) {
+                var paginationItem = document.createElement('LI');
                 if (totalPageCount > maxPaginationLinksToShow && currentPageNumber < totalPageCount - 1) {
                     if (currentPageNumber === maxPaginationLinksToShow - 1) {
-                        pagination.append(
-                            $('<li/>').text('...')
-                        );
+                        paginationItem.textContent = '...';
+                        pagination.appendChild(paginationItem);
                     }
                     continue;
                 }
                 if (currentPageNumber === pageNumber) {
-                    pagination.append(
-                        $('<li/>').addClass('current').text(pageNumber)
-                    );
+                    paginationItem.className = 'current';
+                    paginationItem.textContent = pageNumber.toString();
                 } else {
-                    pagination.append(
-                        $('<li/>').append(
-                            $('<a/>').prop('href', url.updateQueryParameter(paginationQueryParameterName, pageNumber))
-                                .text(pageNumber)
-                        )
-                    );
+                    var paginationLink = document.createElement('A');
+                    paginationLink.textContent = pageNumber.toString();
+                    paginationLink.href = url.updateQueryParameter(paginationQueryParameterName, pageNumber);
+                    paginationItem.appendChild(paginationLink);
                 }
+                pagination.appendChild(paginationItem);
             }
 
             if (totalPageCount && totalPageCount > currentPageNumber) {
-                pagination.append(
-                    $('<li/>').append(
-                        $('<a/>').addClass('next')
-                            .prop('href', url.toggleQueryParameter(paginationQueryParameterName, totalPageCount))
-                            .html('&#9654;')
-                    )
-                );
+                pagination.appendChild(createNextPaginationItem(totalPageCount));
             }
 
-            paginationPlaceholder.append(pagination);
+            paginationPlaceholder.appendChild(pagination);
         }
     }
 });
