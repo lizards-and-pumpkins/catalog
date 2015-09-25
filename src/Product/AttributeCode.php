18e1c8112a33e3d7f724091e43d543c3124d3a90
Vinai Kopp <vinai@netzarbeiter.com>
2015-09-25 17:32:53 +0200

    Issue #229: Add AttributeCode

---
+++ src/Product/AttributeCode.php
@@ -0,0 +1,82 @@
+<?php
+
+
+namespace LizardsAndPumpkins\Product;
+
+use LizardsAndPumpkins\Product\Exception\InvalidAttributeCodeException;
+
+class AttributeCode
+{
+    /**
+     * @var string
+     */
+    private $code;
+
+    /**
+     * @param string $attributeCode
+     */
+    private function __construct($attributeCode)
+    {
+        $this->code = $attributeCode;
+    }
+
+    /**
+     * @param string $attributeCode
+     * @return AttributeCode
+     */
+    public static function fromString($attributeCode)
+    {
+        self::validateAttributeCode($attributeCode);
+        return new self($attributeCode);
+    }
+
+    /**
+     * @param mixed $variable
+     * @return string
+     */
+    private static function getType($variable)
+    {
+        return is_object($variable) ?
+            get_class($variable) :
+            gettype($variable);
+    }
+
+    /**
+     * @param mixed $attributeCode
+     */
+    private static function validateAttributeCode($attributeCode)
+    {
+        if (!is_string($attributeCode)) {
+            $message = sprintf('The attribute code has to be a string, got "%s"', self::getType($attributeCode));
+            throw new InvalidAttributeCodeException($message);
+        }
+        if (empty($attributeCode)) {
+            $message = sprintf('The attribute code has to be at least 3 characters long, got ""', $attributeCode);
+            throw new InvalidAttributeCodeException($message);
+        }
+        $chr = ord(substr($attributeCode, 0, 1));
+        if ($chr < ord('a') || $chr > ord('z')) {
+            $message = sprintf('The first letter of the attribute code has to be a character from a-z, got "%c"', $chr);
+            throw new InvalidAttributeCodeException($message);
+        }
+        if (preg_match('/[^a-z0-9_]/', $attributeCode)) {
+            $message = sprintf(
+                'The attribute code may only contain letters from a-z, numbers and underscores, got "%s"',
+                $attributeCode
+            );
+            throw new InvalidAttributeCodeException($message);
+        }
+        if (substr($attributeCode, -1) === '_') {
+            $message = sprintf('The attribute code may not and with an underscore, got "%s"', $attributeCode);
+            throw new InvalidAttributeCodeException($message);
+        }
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString()
+    {
+        return $this->code;
+    }
+}
---
+++ tests/Unit/Suites/Product/AttributeCodeTest.php
@@ -0,0 +1,83 @@
+<?php
+
+
+namespace LizardsAndPumpkins\Product;
+
+use LizardsAndPumpkins\Product\Exception\InvalidAttributeCodeException;
+
+class AttributeCodeTest extends \PHPUnit_Framework_TestCase
+{
+    public function testItReturnsAnAttributeCodeInstance()
+    {
+        $attributeCode = AttributeCode::fromString('test');
+        $this->assertInstanceOf(AttributeCode::class, $attributeCode);
+    }
+
+    public function testItReturnsTheInjectedCodeWhenCastToString()
+    {
+        $this->assertSame('test_code', (string) AttributeCode::fromString('test_code'));
+    }
+
+    public function testItThrowsAnExceptionIfTheCodeIsNotAString()
+    {
+        $this->setExpectedException(
+            InvalidAttributeCodeException::class,
+            'The attribute code has to be a string, got "integer"'
+        );
+        AttributeCode::fromString(222);
+    }
+
+    public function testItThrowsAnExceptionIfTheAttributeCodeIsLessThenThreeCharactersLong()
+    {
+        $this->setExpectedException(
+            InvalidAttributeCodeException::class,
+            'The attribute code has to be at least 3 characters long, got ""'
+        );
+        AttributeCode::fromString('');
+    }
+
+    /**
+     * @dataProvider attributeCodeWithInvalidFirstCharacterProvider
+     */
+    public function testItThrowsAnExceptionIfTheFirstCharacterIsNotAThroughZ($attributeCode)
+    {
+        $this->setExpectedException(
+            InvalidAttributeCodeException::class,
+            'The first letter of the attribute code has to be a character from a-z, got '
+        );
+        AttributeCode::fromString($attributeCode);
+    }
+
+    /**
+     * @return array[]
+     */
+    public function attributeCodeWithInvalidFirstCharacterProvider()
+    {
+        return [
+            ['Aaaaa'],
+            ['Zaaaa'],
+            ['0aaaa'],
+            ['9aaaa'],
+            ['ßaaaa'],
+            ['.aaaa'],
+        ];
+    }
+
+    public function testItThrowsAnExceptionIfTheAttributeCodeContainsInvalidCharacters()
+    {
+        $this->setExpectedException(
+            InvalidAttributeCodeException::class,
+            'The attribute code may only contain letters from a-z, numbers and underscores, got "abc."'
+        );
+        AttributeCode::fromString('abc.');
+    }
+
+    public function testItThrowsAnExceptionIfTheAttributeCodeEndsWithAnUnderscore()
+    {
+        $this->setExpectedException(
+            InvalidAttributeCodeException::class,
+            'The attribute code may not and with an underscore, got "abc_"'
+        );
+        AttributeCode::fromString('abc_');
+    }
+}
