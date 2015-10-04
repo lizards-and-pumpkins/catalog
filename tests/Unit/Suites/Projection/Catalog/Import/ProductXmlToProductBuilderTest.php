69b3a04a9ce3dd8c33539a8c21c05333c9c18ac1
Vinai Kopp <vinai@netzarbeiter.com>
2015-10-04 14:28:17 +0200

    Issue #229: Make test easier to read

--- tests/Unit/Suites/Projection/Catalog/Import/ProductXmlToProductBuilderTest.php
+++ tests/Unit/Suites/Projection/Catalog/Import/ProductXmlToProductBuilderTest.php
@@ -145,11 +145,13 @@ class ProductXmlToProductBuilderTest extends \PHPUnit_Framework_TestCase
 
     public function testProductBuilderIsCreatedFromXmlIgnoringAssociatedProductAttributes()
     {
-        $secondNode = $this->domDocument->getElementsByTagName('product')->item(1);
-        $secondNodeXml = $this->domDocument->saveXML($secondNode);
+        $configurableProductXml = $this->getConfigurableProductXml();
         
-        $productBuilder = $this->builder->createProductBuilderFromXml($secondNodeXml);
-        $this->assertEmpty($this->getAttributesWithCodeFromInstance($productBuilder, 'size'));
+        $productBuilder = $this->builder->createProductBuilderFromXml($configurableProductXml);
+        $sizeAttributes = $this->getAttributesWithCodeFromInstance($productBuilder, 'size');
+        $this->assertEmpty($sizeAttributes, 'The configurable product builder has "size" attributes');
+        $colorAttributes = $this->getAttributesWithCodeFromInstance($productBuilder, 'color');
+        $this->assertEmpty($colorAttributes, 'The configurable product builder has "color" attributes');
     }
 
     public function testExceptionIsThrownIfSkuIsMissing()
