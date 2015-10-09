ca15523ea035de4febf8a1695327b539ba4d3f82
Tim Bezhashvyly <tim.bezhashvyly@gmail.com>
2015-10-09 22:09:31 +0200

    Issue #411: Rebuild pagination rendering

--- pub/js/pagination.js
+++ pub/js/pagination.js
@@ -1,60 +1,82 @@
-define(['url', 'jquery'], function (url, $) {
+define(['lib/url'], function (url) {
+    var paginationQueryParameterName = 'p',
+        maxNumberOfPagesAroundSelected = 2;
+
+    var createPaginationItemWithLink = function (itemUrl, itemHtml, cssClass) {
+        var item = document.createElement('LI'),
+            link = document.createElement('A');
+        link.className = cssClass;
+        link.href = itemUrl;
+        link.innerHTML = itemHtml;
+        item.appendChild(link);
+
+        return item;
+    };
+
+    var createPaginationItem = function (itemHtml, cssClass) {
+        var item = document.createElement('LI');
+        item.className = cssClass;
+        item.innerHTML = itemHtml;
+
+        return item;
+    };
+
     return {
         generatePagination: function (totalPageCount, paginationPlaceholderSelector) {
-            var paginationPlaceholder = $(paginationPlaceholderSelector);
+            var paginationPlaceholder = document.querySelector(paginationPlaceholderSelector);
 
-            if (!paginationPlaceholderSelector.length) {
+            if (null === paginationPlaceholderSelector) {
                 return;
             }
 
-            var maxPaginationLinksToShow = 7,
-                paginationQueryParameterName = 'p',
-                pagination = $('<ol/>'),
-                currentPageNumber = Math.max(1, url.getQueryParameterValue(paginationQueryParameterName));
+            var pagination = document.createElement('OL'),
+                selectedPageNumber = Math.max(1, url.getQueryParameterValue(paginationQueryParameterName));
 
-            if (totalPageCount && 1 < currentPageNumber) {
-                pagination.append(
-                    $('<li/>').append(
-                        $('<a/>').addClass('prev')
-                            .prop('href', url.updateQueryParameter(paginationQueryParameterName, 1)).html('&#9664;')
-                    )
-                );
+            if (totalPageCount && 1 < selectedPageNumber) {
+                var previousPageUrl = url.updateQueryParameter(paginationQueryParameterName, selectedPageNumber - 1);
+                pagination.appendChild(createPaginationItemWithLink(previousPageUrl, '&#9664;', 'prev'));
             }
 
             for (var pageNumber = 1; pageNumber <= totalPageCount; pageNumber++) {
-                if (totalPageCount > maxPaginationLinksToShow && currentPageNumber < totalPageCount - 1) {
-                    if (currentPageNumber === maxPaginationLinksToShow - 1) {
-                        pagination.append(
-                            $('<li/>').text('...')
-                        );
+                if (selectedPageNumber === pageNumber) {
+                    pagination.appendChild(createPaginationItem(pageNumber.toString(), 'current'));
+                    continue;
+                }
+
+                if (selectedPageNumber - maxNumberOfPagesAroundSelected > 0 && pageNumber == 1) {
+                    var firstPageUrl = url.updateQueryParameter(paginationQueryParameterName, 1);
+                    pagination.appendChild(createPaginationItemWithLink(firstPageUrl, pageNumber.toString(), ''));
+                    if (selectedPageNumber - maxNumberOfPagesAroundSelected - 1 > 1) {
+                        pagination.appendChild(createPaginationItem('...', 'spacing'));
                     }
                     continue;
                 }
-                if (currentPageNumber === pageNumber) {
-                    pagination.append(
-                        $('<li/>').addClass('current').text(pageNumber)
-                    );
-                } else {
-                    pagination.append(
-                        $('<li/>').append(
-                            $('<a/>').prop('href', url.updateQueryParameter(paginationQueryParameterName, pageNumber))
-                                .text(pageNumber)
-                        )
-                    );
+
+                if (selectedPageNumber + maxNumberOfPagesAroundSelected < totalPageCount && pageNumber == totalPageCount) {
+                    var lastPageUrl = url.updateQueryParameter(paginationQueryParameterName, totalPageCount);
+                    if (selectedPageNumber + maxNumberOfPagesAroundSelected + 1 < totalPageCount) {
+                        pagination.appendChild(createPaginationItem('...', 'spacing'));
+                    }
+                    pagination.appendChild(createPaginationItemWithLink(lastPageUrl, pageNumber.toString(), ''));
+                    continue;
                 }
+
+                if (pageNumber < selectedPageNumber - maxNumberOfPagesAroundSelected ||
+                    pageNumber > selectedPageNumber + maxNumberOfPagesAroundSelected
+                ) {
+                    continue;
+                }
+
+                var pageUrl = url.updateQueryParameter(paginationQueryParameterName, pageNumber);
+                pagination.appendChild(createPaginationItemWithLink(pageUrl, pageNumber.toString(), ''));
             }
 
-            if (totalPageCount && totalPageCount > currentPageNumber) {
-                pagination.append(
-                    $('<li/>').append(
-                        $('<a/>').addClass('next')
-                            .prop('href', url.updateQueryParameter(paginationQueryParameterName, totalPageCount))
-                            .html('&#9654;')
-                    )
-                );
+            if (totalPageCount && totalPageCount > selectedPageNumber) {
+                var nextPageUrl = url.updateQueryParameter(paginationQueryParameterName, selectedPageNumber + 1);
+                pagination.appendChild(createPaginationItemWithLink(nextPageUrl, '&#9654;', 'next'));
             }
 
-            paginationPlaceholder.append(pagination);
+            paginationPlaceholder.appendChild(pagination);
         }
     }
 });
