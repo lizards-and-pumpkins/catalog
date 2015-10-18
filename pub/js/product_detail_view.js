6fd14c2d50fc25b116f2226379cd5a273e5133f9
Nina Kushchevska <nina.kuschevska@gmail.com>
2015-10-18 12:25:35 +0200

    Issue #402: Replace jQuery usage in product_detail_view.js w/ pure JS

--- pub/js/product_detail_view.js
+++ pub/js/product_detail_view.js
@@ -4,44 +4,39 @@ require([
     'recently_viewed_products',
     'lib/styleselect',
     'lib/zoom',
-    'lib/swiping_container',
-    'jquery'
-], function(domReady, common, recentlyViewedProducts, styleSelect, zoom, toggleSwipingArrows, jQuery) {
+    'lib/swiping_container'
+], function(domReady, common, recentlyViewedProducts, styleSelect, zoom, toggleSwipingArrows) {
 
     var tabletWidth = 768,
         siteFullWidth = 975;
 
     domReady(function() {
+        styleSelect('select');
+
+        handleRecentlyViewedProducts();
+
+        adjustToPageWidth();
+        window.addEventListener('resize', adjustToPageWidth);
+        window.addEventListener('orientationchange', adjustToPageWidth);
+
+        initializeZoom();
+        initializeTabs();
+        fixIOS5Bug();
+
         require([
             '//connect.facebook.net/de_DE/all.js#xfbml=1&status=0',
             '//platform.twitter.com/widgets.js',
             '//apis.google.com/js/plusone.js'
         ]);
-
-//        $('select').not('.alertPopUp select').uniform({selectAutoWidth: false});
-        styleSelect('select');
-
-        handleRecentlyViewedProducts();
-
-        adjustDetailsToWidth();
-        jQuery(window).bind('resize orientationchange', adjustDetailsToWidth);
-
-        initializeZoom();
-        initializeTabs();
-        observeSizeDropDown();
-        //initiateOverlays();
-        initiateFullScreenImageViewer();
-        initiateYouTubePlayer();
-        initiate360View();
-        fixIOS5Bug();
     });
 
     function handleRecentlyViewedProducts() {
         var recentlyViewedProductsListHtml = recentlyViewedProducts.getRecentlyViewedProductsHtml(product);
 
-        if (-1 != recentlyViewedProductsListHtml.indexOf('</li>')) {
-            jQuery('#recently-viewed-products').show()
-                .find('.swipe-container').eq(0).html(recentlyViewedProductsListHtml);
+        if (recentlyViewedProductsListHtml.indexOf('</li>') !== -1) {
+            var container = document.querySelector('#recently-viewed-products .swipe-container');
+            container.innerHTML = recentlyViewedProductsListHtml;
+            container.parentNode.style.display = 'block';
         }
 
         recentlyViewedProducts.addProductIntoLocalStorage(product);
@@ -52,278 +47,152 @@ require([
     }
 
     function initializeTabs() {
-        jQuery('ul.tabs').each(function() {
-            var links = jQuery(this).find('a'),
-                active = jQuery(links.filter('[href="' + location.hash + '"]')[0] || links[0]).addClass('active'),
-                content = jQuery(active[0].hash);
+        var activeTab,
+            activeTabContent;
 
-            links.not(active).each(function () {
-                jQuery(this.hash).hide();
-            });
+        Array.prototype.map.call(document.querySelectorAll('ul.tabs a'), function (tabLink, index) {
+            if (0 === index) {
+                activeTab = tabLink;
+                activeTab.className = 'active';
 
-            jQuery(this).on('click', 'a', function(e){
-                active.removeClass('active');
-                content.hide();
-
-                active = jQuery(this);
-                content = jQuery(this.hash);
-
-                active.addClass('active');
-                content.show();
-
-                e.preventDefault();
-            });
-        });
-    }
-
-    function observeSizeDropDown() {
-        jQuery('div#productView select').not('#qty').eq(0).bind('change', function (event) {
-            var selected_item = jQuery(event.target).find(':selected'),
-                selected_item_sold_out = !jQuery(event.target).length ||
-                    typeof selected_item.attr('disabled') != 'undefined' ||
-                    jQuery(event.target).prop('selectedIndex') == 0,
-                qty_box = jQuery('.qty-box'),
-                buttonInstance = jQuery('#productView').find('button[type=submit]'),
-                selectedItemStockAvailable = selected_item.attr('stock');
-
-            jQuery('.product-shop span.availability').hide();
-            if (selectedItemStockAvailable && selectedItemStockAvailable.match(/[0-9]+/)) {
-                jQuery('.product-shop span.selectedSize').html(selected_item.text());
-                jQuery('.product-shop span.availability' + (selected_item_sold_out ? '.false' : '.true')).show();
-                var qty = qty_box.find('select');
-                qty.find('option').remove();
-                for (var i = 0; i < selectedItemStockAvailable; i++) {
-                    qty.append(jQuery('<option>' + (i + 1) + '</option>'));
-                }
-                styleSelect('.qty-box select');
-            } else {
-                jQuery('.product-shop span.availability.choose').show();
-                jQuery('.product-shop .selectedSize').html('');
+                activeTabContent = document.getElementById(activeTab.hash.slice(1));
+                activeTabContent.style.display = 'block';
             }
-            selected_item_sold_out ? qty_box.hide() : qty_box.show();
-            selected_item_sold_out ? buttonInstance.attr('disabled', 'disabled') : buttonInstance.removeAttr('disabled');
-        }).trigger('change');
-    }
 
-    function initiateOverlays() {
-        jQuery('.product-shop .productAlert').bind('click', function () {
-            /* Show popup */
-            new ModalBox().show(jQuery('#alertLayer').html());
+            tabLink.addEventListener('click', function (event) {
+                event.preventDefault();
 
-            styleSelect('.modal-popup .alertPopUp select');
+                activeTab.className = '';
+                activeTabContent.style.display = 'none';
 
-            /* Bind action to button */
-            jQuery('.alertPopUp button').on('click', function () {
-                location.href = jQuery(this).parents('.alertPopUp').find('select').val();
-            });
+                activeTab = event.target;
+                activeTab.className = 'active';
+
+                activeTabContent = document.getElementById(activeTab.hash.slice(1));
+                activeTabContent.style.display = 'block';
+            }, true);
         });
-        jQuery('.availability .info').bind('click', function () {
-            new ModalBox().show(Mage.baseUrl + 'common/index/index/?identifier=versandinfo');
-        });
-    }
-
-    function initiateFullScreenImageViewer() {
-        if (typeof(slideshowSwf) == 'undefined') return;
-        jQuery('div#productView div.slideshow').flash({
-            swf: slideshowSwf,
-            allowfullscreen: true,
-            flashvars: {
-                data: JSON.stringify(slideshowData)
-            }
-        });
-    }
-
-    function initiateYouTubePlayer() {
-        var holder = jQuery('div#productView div.youTubeHolder');
-        jQuery('div#productView div.product-essential div.product-img-box div.more-views a.youtube').click(function () {
-            hideSwfObjects();
-            var youTubeOpts = {
-                swf: 'http://www.youtube.com/v/' + youTubeLink + '?version=3&&autohide=1&autoplay=1&egm=1&hd=1&modestbranding=1&rel=0&showsearch=0',
-                id: 'player',
-                width: mediaWidth,
-                height: mediaHeight,
-                allowfullscreen: true,
-                flashvars: {
-                    autohide: true
-                }
-            };
-            holder.flash(youTubeOpts);
-            holder.show();
-        });
-    }
-
-    function initiate360View() {
-        jQuery('div#productView div.product-essential div.product-img-box div.more-views a.panorama').click(function () {
-            hideSwfObjects();
-            var panorama = jQuery('div#productView div.panorama');
-            panorama.flash({
-                swf: panorama_swf,
-                width: mediaWidth,
-                height: mediaHeight
-            });
-            panorama.show();
-        });
-    }
-
-    function hideSwfObjects() {
-        jQuery('div#productView div.panorama, div#productView div.youTubeHolder, div#productView div.slideshow, div#productView div.main-image-area-wrapper div#wrap div.mousetrap').hide();
-        jQuery('div#productView div.panorama, div#productView div.youTubeHolder').find('object, embed').detach();
     }
 
     function fixIOS5Bug() {
-        /**
-         * iOS5 has a problem disabling <select/> elements. Hence such elements are removed if such device is detected.
-         */
         if (navigator.userAgent.match(/\(iP[^)]+OS\s5/)) {
-            jQuery('#switcher-container').find('option:disabled').remove();
+            var disabledOptions = document.querySelectorAll('#switcher-container option:disabled');
+            Array.prototype.map.call(disabledOptions, function (option) {
+                option.parentNode.removeChild(option);
+            });
         }
     }
 
-    function adjustDetailsToWidth() {
-        var currentWidth = jQuery(window).width(),
+    function adjustToPageWidth() {
+        var currentWidth = document.body.clientWidth,
         /* Maybe it makes sense to initialize variables on load only ? */
-            phoneTitlePlaceholder = jQuery('#phoneTitlePlaceholder'),
-            productTitle = jQuery('.product-title-info h1'),
-            brandLogo = jQuery('.product-title img'),
-            socialIcons = jQuery('.socialSharing'),
-            productTopContainer = jQuery('.product-shop > .top'),
-            productControls = jQuery('.product-controls'),
-            price = jQuery('.price-information'),
-            productMainInfo = jQuery('.product-main-info'),
-            similarProductsLink = jQuery('.similarProducts'),
-            articleInformations = jQuery('.articleInformations'),
-            productShopColumn = jQuery('.product-shop'),
-            productImageColumn = jQuery('.product-img-box'),
-            essentialsContainer = jQuery('.product-essential');
+            productTitle = document.querySelector('.product-essential h1'),
+            brandLogo = document.getElementById('brandLogo'),
+            socialIcons = document.querySelector('.socialSharing'),
+            productTopContainer = document.querySelector('.product-shop > .top'),
+            productControls = document.querySelector('.product-controls'),
+            price = document.querySelector('.price-information'),
+            productMainInfo = document.querySelector('.product-main-info'),
+            similarProductsLink = document.querySelector('.similarProducts'),
+            articleInformation = document.querySelector('.articleInformations');
 
         /* Phone only */
         if (currentWidth < tabletWidth) {
+            var phoneTitlePlaceholder = document.getElementById('phoneTitlePlaceholder');
 
-            /* Create product title placeholder if not exists */
-            if (!phoneTitlePlaceholder.length) {
-                phoneTitlePlaceholder = jQuery('<div/>', {
-                    'id': 'phoneTitlePlaceholder'
-                });
-                essentialsContainer.prepend(phoneTitlePlaceholder);
+            if (!isParent(phoneTitlePlaceholder, productTitle)) {
+                phoneTitlePlaceholder.appendChild(productTitle);
             }
 
-            /* Move product title if required */
-            if (!phoneTitlePlaceholder.has(productTitle).length) {
-                phoneTitlePlaceholder.append(productTitle);
+            if (!isParent(phoneTitlePlaceholder, brandLogo)) {
+                phoneTitlePlaceholder.appendChild(brandLogo);
             }
 
-            /* Move brand logo if required */
-            if (!phoneTitlePlaceholder.has(brandLogo).length) {
-                phoneTitlePlaceholder.append(brandLogo);
+            if (!isParent(productTopContainer, price)) {
+                productTopContainer.appendChild(price);
             }
 
-            /* Move social icons if required */
-            if (!productControls.has(socialIcons).length) {
-                socialIcons.insertBefore(jQuery('.otherLinks'));
+            if (!isParent(productControls, similarProductsLink)) {
+                productControls.appendChild(similarProductsLink);
             }
 
-            /* Move price if required */
-            if (!productTopContainer.has(price).length) {
-                productTopContainer.prepend(price);
+            if (!isParent(productControls, articleInformation)) {
+                productControls.appendChild(articleInformation);
             }
 
-            /* Move similar product link if required */
-            if (!productControls.has(similarProductsLink).length) {
-                similarProductsLink.insertBefore(socialIcons);
+            if (!isParent(productControls, socialIcons)) {
+                productControls.appendChild(socialIcons);
             }
 
-            /* Move SKU if required */
-            if (!productControls.has(articleInformations).length) {
-                articleInformations.insertBefore(socialIcons);
-            }
-
-            /* Create images list if required */
-            var imagesList = jQuery('#images-carousel');
-            if (!imagesList.length) {
-                imagesList = jQuery('<ul/>', {'id': 'images-carousel', 'class': 'slides'});
-                jQuery('.more-views > ul > li > a').each(function () {
-                    var src = jQuery(this).attr('href');
-                    if (src && src.match(/^http/)) {
-                        imagesList.append(jQuery('<li/>').append(jQuery('<img/>', {'src': src})));
-                    }
-                });
-                productImageColumn.prepend(jQuery('<div/>', {'class': 'flexslider'}).append(imagesList));
-//                jQuery('.flexslider').flexslider({'slideshow': false, 'animationLoop': false});
-            }
+            /* TODO: Implement image slider */
 
             /* Hide "send" part of FB buttons block if not yet hidden */
             fbEnsureInit(processFbButton);
         } else {
-            var originalTitleContainer = jQuery('.product-title-info');
+            var originalTitleContainer = document.querySelector('.product-title');
 
-            /* Move product title back if required */
-            if (!productTitle.length) {
-                originalTitleContainer.prepend(phoneTitlePlaceholder.find('h1'));
+            if (!isParent(originalTitleContainer, brandLogo)) {
+                originalTitleContainer.appendChild(brandLogo);
             }
 
-            /* Move brand logo back if required */
-            if (!brandLogo.length) {
-                originalTitleContainer.parent().prepend(phoneTitlePlaceholder.find('img'));
+            if (!isParent(originalTitleContainer, productTitle)) {
+                originalTitleContainer.appendChild(productTitle);
             }
 
-            /* Move social icons back if required */
-            if (!productTopContainer.has(socialIcons).length) {
-                productTopContainer.append(socialIcons);
+            if (!isParent(originalTitleContainer, similarProductsLink)) {
+                originalTitleContainer.appendChild(similarProductsLink);
             }
 
-            /* Move price back if required */
-            if (!productMainInfo.has(price).length) {
-                price.insertAfter(productMainInfo.find('.main-information'));
+            if (!isParent(productTopContainer, articleInformation)) {
+                productTopContainer.appendChild(articleInformation);
             }
 
-            /* Move similar product link back if required */
-            if (!originalTitleContainer.has(similarProductsLink).length) {
-                originalTitleContainer.append(similarProductsLink);
+            if (!isParent(productTopContainer, socialIcons)) {
+                productTopContainer.appendChild(socialIcons);
             }
 
-            /* Move SKU back if required */
-            if (!originalTitleContainer.parent().has(articleInformations).length) {
-                articleInformations.insertBefore(originalTitleContainer.parent());
+            if (!isParent(productMainInfo, price)) {
+                productMainInfo.appendChild(price);
             }
 
             /* Revert "send" part of FB buttons block if not yet recovered */
             fbEnsureInit(processFbButton);
         }
 
-        /* Reset .product-shop price */
-        productShopColumn.width('');
-
         /* Tablet only */
         if (currentWidth < siteFullWidth && currentWidth >= tabletWidth) {
 
-            /* Constantly update .product-shop price */
-            productShopColumn.width(essentialsContainer.outerWidth() - productImageColumn.outerWidth() - 30);
-
-            /* Move price if required */
-            if (!productTopContainer.has(price).length) {
-                productTopContainer.append(price);
+            if (!isParent(productTopContainer, price)) {
+                productTopContainer.appendChild(price);
             }
 
-            /* Move SKU if required */
-            if (!productControls.has(articleInformations).length) {
-                articleInformations.insertAfter(socialIcons);
+            if (!isParent(productTopContainer, articleInformation)) {
+                productTopContainer.appendChild(articleInformation);
             }
 
         } else if (currentWidth >= siteFullWidth) {
 
-            /* Move price back if required */
-            if (!productMainInfo.has(price).length) {
-                price.insertAfter(productMainInfo.find('.main-information'));
+            if (!isParent(productMainInfo, price)) {
+                productMainInfo.appendChild(price);
             }
 
-            /* Move SKU back if required */
-            if (!originalTitleContainer.parent().has(articleInformations).length) {
-                articleInformations.insertBefore(originalTitleContainer.parent());
+            if (!isParent(productTopContainer, articleInformation)) {
+                productTopContainer.appendChild(articleInformation);
             }
-
         }
 
         toggleSwipingArrows('.swipe-container', 'ul');
+    }
+
+    function isParent(parent, child) {
+        var node = child.parentNode;
+        while (node != null) {
+            if (node == parent) {
+                return true;
+            }
+            node = node.parentNode;
+        }
+        return false;
     }
 
     /**
@@ -331,25 +200,23 @@ require([
      */
     function fbEnsureInit(callback) {
         if (typeof FB == 'undefined') {
-            setTimeout(function () {
-                fbEnsureInit(callback)
-            }, 50);
+            setTimeout(function () { fbEnsureInit(callback) }, 50);
         } else if (callback) {
             callback();
         }
     }
 
     function processFbButton() {
-        var fbContainer = jQuery('.fb-like');
+        var fbContainer = document.querySelector('.fb-like');
 
-        if (jQuery(window).width() < tabletWidth) {
-            if (fbContainer.attr('data-send')) {
-                fbContainer.removeAttr('data-send');
+        if (document.body.clientWidth < tabletWidth) {
+            if (fbContainer.getAttribute('data-send')) {
+                fbContainer.removeAttribute('data-send');
                 FB.XFBML.parse();
             }
         } else {
-            if (typeof fbContainer == 'undefined' || typeof fbContainer.attr('data-send') == 'undefined') {
-                fbContainer.attr('data-send', true);
+            if (typeof fbContainer == 'undefined' || typeof fbContainer.getAttribute('data-send') == 'undefined') {
+                fbContainer.setAttribute('data-send', true);
                 FB.XFBML.parse();
             }
         }
