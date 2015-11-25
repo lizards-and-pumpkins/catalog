53d2557d3a7c6b473190aa271a9798b9e7fbb5ad
Vinai Kopp <vinai@netzarbeiter.com>
2015-11-25 10:43:17 +0100

    Issue #487: Implement simple, self-contained context

---
+++ tests/Unit/Suites/Context/CompleteContextTest.php
@@ -0,0 +1,144 @@
+<?php
+
+
+namespace LizardsAndPumpkins\Context;
+
+use LizardsAndPumpkins\Context\Exception\ContextCodeNotFoundException;
+
+class CompleteContextTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @param string[] $data
+     * @return CompleteContext
+     */
+    private function createContext(array $data)
+    {
+        return CompleteContext::fromArray($data);
+    }
+    
+    public function testItImplementsTheContextInterface()
+    {
+        $this->assertInstanceOf(Context::class, $this->createContext([]));
+    }
+
+    public function testItReturnsAStringRepresentationContainingTheDataParts()
+    {
+        $this->assertSame('key1:value1', (string) $this->createContext(['key1' => 'value1']));
+        $this->assertSame(
+            'key1:value1_key2:value2',
+            (string) $this->createContext(['key1' => 'value1', 'key2' => 'value2'])
+        );
+    }
+
+    public function testItExtractsTheGivenParts()
+    {
+        $this->assertSame('key1:value1', (string) $this->createContext(['key1' => 'value1'])->getIdForParts(['key1']));
+        $this->assertSame(
+            'key2:value2',
+            (string) $this->createContext(['key1' => 'value1', 'key2' => 'value2'])->getIdForParts(['key2'])
+        );
+        $contextData = ['key1' => 'value1', 'key2' => 'value2', 'key3' => 'value3', 'key4' => 'value4'];
+        $this->assertSame(
+            'key2:value2_key4:value4',
+            (string) $this->createContext($contextData)->getIdForParts(['key2', 'key4'])
+        );
+    }
+
+    public function testItThrowsAnExceptionIfTheRequestedPartIsNotPresent()
+    {
+        $this->setExpectedException(
+            ContextCodeNotFoundException::class,
+            'No value found in the current context for the code "test"'
+        );
+        $this->createContext([])->getValue('test');
+    }
+
+    public function testItReturnsTheValueForTheGivenCode()
+    {
+        $this->assertSame('value', $this->createContext(['key' => 'value'])->getValue('key'));
+        $this->assertSame('value2', $this->createContext(['key1' => 'value1', 'key2' => 'value2'])->getValue('key2'));
+    }
+
+    public function testItReturnsTheSupportedContextPartCodes()
+    {
+        $this->assertSame([], $this->createContext([])->getSupportedCodes());
+        $this->assertSame(['key'], $this->createContext(['key' => 'value'])->getSupportedCodes());
+    }
+
+    public function testItReturnsFalseIfTheGivenContextPartCodeIsNotPresent()
+    {
+        $this->assertFalse($this->createContext([])->supportsCode('key'));
+    }
+
+    public function testItReturnsTrueIfTheGivenContextPartCodeIsPresent()
+    {
+        $this->assertTrue($this->createContext(['key' => 'value'])->supportsCode('key'));
+        $this->assertTrue($this->createContext(['key' => '0'])->supportsCode('key'));
+        $this->assertTrue($this->createContext(['key' => ''])->supportsCode('key'));
+    }
+
+    public function testItIsNotASubsetIfTheGivenContextHasCodesThatWeDoNotHave()
+    {
+        $contextA = $this->createContext(['key1' => 'value1']);
+        $contextB = $this->createContext(['key2' => 'value2']);
+        
+        $this->assertFalse($contextB->isSubsetOf($contextA));
+    }
+
+    public function testItIsNotASubsetIfTheGivenContextHasCodesThatWeHaveButTheValueIsDifferent()
+    {
+        $contextA = $this->createContext(['key1' => 'value1']);
+        $contextB = $this->createContext(['key1' => 'value2']);
+        
+        $this->assertFalse($contextB->isSubsetOf($contextA));
+    }
+
+    public function testItIsASubsetIfTheGivenContextHasOnlyCodesThatWeAlsoHaveAndTheValuesAreTheSame()
+    {
+        $contextA = $this->createContext(['key1' => 'value1']);
+        $contextB = $this->createContext(['key1' => 'value1']);
+        
+        $this->assertTrue($contextB->isSubsetOf($contextA));
+    }
+
+    public function testItIsASubsetIfTheGivenContextHasMorePartsButTheSamePartsMatch()
+    {
+        $contextA = $this->createContext(['key1' => 'value1', 'key2' => 'value2']);
+        $contextB = $this->createContext(['key1' => 'value1']);
+
+        $this->assertTrue($contextB->isSubsetOf($contextA));
+    }
+
+    public function testItIsNotASubsetIfTheGivenContextLessParts()
+    {
+        $contextA = $this->createContext(['key1' => 'value1']);
+        $contextB = $this->createContext(['key1' => 'value1', 'key2' => 'value2']);
+
+        $this->assertFalse($contextB->isSubsetOf($contextA));
+    }
+
+    public function testItDoesNotMatchADataSetMissingPartsOfTheContext()
+    {
+        $this->assertFalse($this->createContext(['key1' => 'value1'])->matchesDataSet(['key2' => 'value2']));
+        $context = $this->createContext(['key1' => 'value1', 'key2' => 'value2']);
+        $this->assertFalse($context->matchesDataSet(['key1' => 'value1']));
+    }
+
+    public function testItDoesNotMatchADataSetWithAllPartsOfTheContextButValuesAreDifferent()
+    {
+        $this->assertFalse($this->createContext(['key1' => 'value1'])->matchesDataSet(['key1' => 'XXX']));
+        $context = $this->createContext(['key1' => 'value1', 'key2' => 'value2']);
+        $this->assertFalse($context->matchesDataSet(['key1' => 'value1', 'key2' => 'XXX']));
+    }
+
+    public function testItMatchesAContextIfAllPartsArePresetntAndTheValuesMatch()
+    {
+        $this->assertTrue($this->createContext(['key1' => 'value1'])->matchesDataSet(['key1' => 'value1']));
+    }
+
+    public function testItReturnsTheContextPartsArrayToJsonSerialize()
+    {
+        $contextParts = ['key1' => 'value1', 'key2' => 'value2'];
+        $this->assertSame($contextParts, $this->createContext($contextParts)->jsonSerialize());
+    }
+}
