07ab24554beb8455ad22515cf8b2ae0706b0dd41
Vinai Kopp <vinai@netzarbeiter.com>
2015-11-27 14:45:48 +0100

    Issue #224: Implement ProductTaxClassSnippetRenderer

---
+++ src/Product/ProductTaxClassSnippetRenderer.php
@@ -0,0 +1,35 @@
+<?php
+
+
+namespace LizardsAndPumpkins\Product;
+
+use LizardsAndPumpkins\Snippet;
+use LizardsAndPumpkins\SnippetKeyGenerator;
+use LizardsAndPumpkins\SnippetList;
+use LizardsAndPumpkins\SnippetRenderer;
+
+class ProductTaxClassSnippetRenderer implements SnippetRenderer
+{
+    /**
+     * @var SnippetList
+     */
+    private $snippetList;
+    
+    /**
+     * @var SnippetKeyGenerator
+     */
+    private $keyGenerator;
+
+    public function __construct(SnippetList $snippetList, SnippetKeyGenerator $keyGenerator)
+    {
+        $this->snippetList = $snippetList;
+        $this->keyGenerator = $keyGenerator;
+    }
+    
+    public function render(Product $product)
+    {
+        $key = $this->keyGenerator->getKeyForContext($product->getContext(), [Product::ID => $product->getId()]);
+        $snippet = Snippet::create($key, $product->getTaxClass());
+        $this->snippetList->add($snippet);
+    }
+}
---
+++ tests/Unit/Suites/Product/ProductTaxClassSnippetRendererTest.php
@@ -0,0 +1,73 @@
+<?php
+
+
+namespace LizardsAndPumpkins\Product;
+
+use LizardsAndPumpkins\Context\Context;
+use LizardsAndPumpkins\Snippet;
+use LizardsAndPumpkins\SnippetKeyGenerator;
+use LizardsAndPumpkins\SnippetList;
+use LizardsAndPumpkins\SnippetRenderer;
+
+class ProductTaxClassSnippetRendererTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var ProductTaxClassSnippetRenderer
+     */
+    private $snippetRenderer;
+
+    /**
+     * @var Product|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private $stubProduct;
+
+    /**
+     * @var SnippetList|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private $mockSnippetList;
+
+    /**
+     * @var SnippetKeyGenerator|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private $mockSnippetKeyGenerator;
+
+    protected function setUp()
+    {
+        $this->mockSnippetList = $this->getMock(SnippetList::class);
+        $this->mockSnippetKeyGenerator = $this->getMock(SnippetKeyGenerator::class);
+        
+        $this->snippetRenderer = new ProductTaxClassSnippetRenderer(
+            $this->mockSnippetList,
+            $this->mockSnippetKeyGenerator
+        );
+        $this->stubProduct = $this->getMock(Product::class);
+    }
+    
+    public function testItIsASnippetRenderer()
+    {
+        $this->assertInstanceOf(SnippetRenderer::class, $this->snippetRenderer);
+    }
+
+    public function testItAddsASnippetWithTheProductTaxClass()
+    {
+        $dummyTaxClass = 'test';
+        $dummyTaxClassSnippetKey = 'test-key';
+        $stubContext = $this->getMock(Context::class);
+
+        /** @var Product|\PHPUnit_Framework_MockObject_MockObject $mockProduct */
+        $mockProduct = $this->getMock(Product::class);
+        $mockProduct->method('getTaxClass')->willReturn($dummyTaxClass);
+        $mockProduct->method('getContext')->willReturn($stubContext);
+
+        $this->mockSnippetKeyGenerator->method('getKeyForContext')
+            ->willReturn($dummyTaxClassSnippetKey);
+
+        $expectedSnippet = Snippet::create($dummyTaxClassSnippetKey, $dummyTaxClass);
+
+        $this->mockSnippetList->expects($this->once())
+            ->method('add')
+            ->with($expectedSnippet);
+
+        $this->snippetRenderer->render($mockProduct);
+    }
+}
