5709908bb6840e3011f99f31f011abb008fa7835
Vinai Kopp <vinai@netzarbeiter.com>
2015-12-03 21:23:18 +0100

    Issue #224: Add missing return type hints

--- tests/Integration/Util/IntegrationTestTaxableCountries.php
+++ tests/Integration/Util/IntegrationTestTaxableCountries.php
@@ -6,12 +6,18 @@ namespace LizardsAndPumpkins;
 class IntegrationTestTaxableCountries implements TaxableCountries
 {
     private static $countries = ['DE', 'FR'];
-    
+
+    /**
+     * @return \ArrayIterator
+     */
     public function getIterator()
     {
         return new \ArrayIterator(self::$countries);
     }
 
+    /**
+     * @return string[]
+     */
     public function getCountries()
     {
         return self::$countries;
