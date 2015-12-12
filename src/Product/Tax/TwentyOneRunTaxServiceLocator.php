a03695aea280e1e0078462fc280ea20fb5e119d3
Vinai Kopp <vinai@netzarbeiter.com>
2015-12-12 04:14:59 -0500

    Issue #224: Refactored to use table based rule map for better readability

--- src/Product/Tax/TwentyOneRunTaxServiceLocator.php
+++ src/Product/Tax/TwentyOneRunTaxServiceLocator.php
@@ -5,105 +5,57 @@ namespace LizardsAndPumpkins\Product\Tax;
 use LizardsAndPumpkins\Country\Country;
 use LizardsAndPumpkins\Product\Tax\Exception\UnableToLocateTaxServiceException;
 use LizardsAndPumpkins\Product\Tax\TaxRates\TwentyOneRunTaxRate;
-use LizardsAndPumpkins\Product\Tax\TaxRates\TwentyOneRunTaxRateService19;
-use LizardsAndPumpkins\Product\Tax\TaxRates\TwentyOneRunTaxRateService20;
-use LizardsAndPumpkins\Product\Tax\TaxRates\TwentyOneRunTaxRateService25;
 use LizardsAndPumpkins\Website\Website;
 
 class TwentyOneRunTaxServiceLocator implements TaxServiceLocator
 {
+    private static $rateTable = [
+        [['ru', 'fr'], ['19%'], 'DE', 19],
+        [['ru', 'fr'], ['7%'], 'DE', 7],
+        [['ru', 'fr'], ['19%', '7%'], 'DK', 25],
+        [['ru', 'fr'], ['19%', '7%'], 'AT', 20],
+        [['ru', 'fr'], ['19%', '7%'], 'FR', 20],
+        [['ru', 'fr'], ['19%', '7%'], 'ES', 21],
+        [['ru', 'fr'], ['19%', '7%'], 'FI', 24],
+        [['ru', 'fr'], ['19%', '7%'], 'NL', 21],
+        [['ru', 'fr'], ['19%', '7%'], 'SE', 25],
+        [['ru', 'fr'], ['19%', '7%'], 'LU', 17],
+        [['ru', 'fr'], ['19%', '7%'], 'IT', 21],
+        [['ru', 'fr'], ['19%', '7%'], 'BE', 21],
+        [['cy'], ['21cycles.com'], 'DE', 19],
+        [['cy'], ['VR 7%'], 'DE', 7],
+    ];
+
     /**
      * @param TaxServiceLocatorOptions $options
      * @return TaxService
      */
     public function get(TaxServiceLocatorOptions $options)
     {
-        if ($this->isWebsite21Run_DE_or_FR($options)) {
-            if ('19%' === $this->getProductTaxClass($options)) {
-                switch ($this->getCountry($options)) {
-                    case 'DE':
-                        return TwentyOneRunTaxRate::create(19);
-                        
-                    case 'DK':
-                        return TwentyOneRunTaxRate::create(25);
-                        
-                    case 'AT':
-                        return TwentyOneRunTaxRate::create(20);
-                    
-                    case 'FR':
-                        return TwentyOneRunTaxRate::create(20);
-                    
-                    case 'ES':
-                        return TwentyOneRunTaxRate::create(21);
-                    
-                    case 'FI':
-                        return TwentyOneRunTaxRate::create(24);
-                    
-                    case 'NL':
-                        return TwentyOneRunTaxRate::create(21);
-                    
-                    case 'SE':
-                        return TwentyOneRunTaxRate::create(25);
-                    
-                    case 'LU':
-                        return TwentyOneRunTaxRate::create(17);
-                    
-                    case 'IT':
-                        return TwentyOneRunTaxRate::create(21);
-                    
-                    case 'BE':
-                        return TwentyOneRunTaxRate::create(21);
-                }
-            }
-            if ('7%' === $this->getProductTaxClass($options)) {
-                switch ($this->getCountry($options)) {
-                    case 'DE':
-                        return TwentyOneRunTaxRate::create(7);
-                    
-                    case 'DK':
-                        return TwentyOneRunTaxRate::create(25);
-                    
-                    case 'AT':
-                        return TwentyOneRunTaxRate::create(20);
-                    
-                    case 'FR':
-                        return TwentyOneRunTaxRate::create(20);
-                    
-                    case 'ES':
-                        return TwentyOneRunTaxRate::create(21);
-                    
-                    case 'FI':
-                        return TwentyOneRunTaxRate::create(24);
-                    
-                    case 'NL':
-                        return TwentyOneRunTaxRate::create(21);
-                    
-                    case 'SE':
-                        return TwentyOneRunTaxRate::create(25);
-                    
-                    case 'LU':
-                        return TwentyOneRunTaxRate::create(17);
-                    
-                    case 'IT':
-                        return TwentyOneRunTaxRate::create(21);
-                    
-                    case 'BE':
-                        return TwentyOneRunTaxRate::create(21);
-                }
-            }
-        }
-        if ('cy' === $this->getWebsite($options)) {
-            switch ($this->getProductTaxClass($options)) {
-                case '21cycles.com':
-                    return TwentyOneRunTaxRate::create(19);
-                
-                case 'VR 7%':
-                    return TwentyOneRunTaxRate::create(7);
+        foreach (self::$rateTable as $rule) {
+            if ($this->isMatchingRule($rule, $options)) {
+                return TwentyOneRunTaxRate::create($rule[$rateIdx = 3]);
             }
         }
         throw $this->createUnableToLocateServiceException($options);
     }
 
+    /**
+     * @param mixed[] $rule
+     * @param TaxServiceLocatorOptions $options
+     * @return bool
+     */
+    private function isMatchingRule(array $rule, TaxServiceLocatorOptions $options)
+    {
+        $websiteIdx = 0;
+        $taxClassIdx = 1;
+        $countryIdx = 2;
+        return
+            in_array($this->getWebsite($options), $rule[$websiteIdx]) &&
+            in_array($this->getProductTaxClass($options), $rule[$taxClassIdx]) &&
+            $this->getCountry($options) === $rule[$countryIdx];
+    }
+    
     /**
      * @param TwentyOneRunTaxServiceLocatorOptions $options
      * @return Country
@@ -129,27 +81,6 @@ class TwentyOneRunTaxServiceLocator implements TaxServiceLocator
     private function getWebsite(TwentyOneRunTaxServiceLocatorOptions $options)
     {
         return (string) $options->getWebsite();
-    }
-
-    /**
-     * @param TaxServiceLocatorOptions $options
-     * @return string
-     */
-    private function getSwitchStatementKey(TaxServiceLocatorOptions $options)
-    {
-        $website = $this->getWebsite($options);
-        $productTaxClass = $this->getProductTaxClass($options);
-        $country = $this->getCountry($options);
-        return $website . '-' . $productTaxClass . '-' . $country;
-    }
-
-    /**
-     * @param TaxServiceLocatorOptions $options
-     * @return bool
-     */
-    private function isWebsite21Run_DE_or_FR(TaxServiceLocatorOptions $options)
-    {
-        return in_array($this->getWebsite($options), ['ru', 'fr']);
     }
 
     /**
