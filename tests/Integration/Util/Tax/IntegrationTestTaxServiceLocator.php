29cfb3c1ec9ee366157fefd958faaeb2b3cb2f09
Vinai Kopp <vinai@netzarbeiter.com>
2015-12-13 23:02:55 +0100

    Issue #224: Import classes

--- tests/Integration/Util/Tax/IntegrationTestTaxServiceLocator.php
+++ tests/Integration/Util/Tax/IntegrationTestTaxServiceLocator.php
@@ -2,14 +2,15 @@
 
 namespace LizardsAndPumpkins\Tax;
 
+use LizardsAndPumpkins\Product\Tax\TaxService;
 use LizardsAndPumpkins\Product\Tax\TaxServiceLocator;
 use LizardsAndPumpkins\Product\Tax\TaxServiceLocatorOptions;
 
 class IntegrationTestTaxServiceLocator implements TaxServiceLocator
 {
     /**
-     * @param \LizardsAndPumpkins\Product\Tax\TaxServiceLocatorOptions $options
-     * @return \LizardsAndPumpkins\Product\Tax\TaxService
+     * @param TaxServiceLocatorOptions $options
+     * @return TaxService
      */
     public function get(TaxServiceLocatorOptions $options)
     {
