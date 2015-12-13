145dd64f34a1668f01aa2487a2fba19df966c681
Vinai Kopp <vinai@netzarbeiter.com>
2015-12-13 22:51:58 +0100

    Issue #224: Remove excessive newlines before namespace declaration

--- src/Product/Tax/TaxService.php
+++ src/Product/Tax/TaxService.php
@@ -1,6 +1,5 @@
 <?php
 
-
 namespace LizardsAndPumpkins\Product\Tax;
 
 use LizardsAndPumpkins\Product\Price;
--- src/Product/Tax/TaxServiceLocatorOptions.php
+++ src/Product/Tax/TaxServiceLocatorOptions.php
@@ -1,6 +1,5 @@
 <?php
 
-
 namespace LizardsAndPumpkins\Product\Tax;
 
 interface TaxServiceLocatorOptions
