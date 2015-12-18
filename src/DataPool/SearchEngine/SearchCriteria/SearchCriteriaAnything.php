9507e31441c70ddb4c1143e62fa32c8138aeb130
Vinai Kopp <vinai@netzarbeiter.com>
2015-12-18 21:11:01 +0100

    Issue #507: Implement SearchCriteriaAnything matching all search documents

--- src/DataPool/SearchEngine/SearchCriteria/SearchCriteriaAnything.php
+++ src/DataPool/SearchEngine/SearchCriteria/SearchCriteriaAnything.php
@@ -2,6 +2,40 @@
 
 namespace LizardsAndPumpkins\DataPool\SearchEngine\SearchCriteria;
 
-class SearchCriteriaAnything
+use LizardsAndPumpkins\DataPool\SearchEngine\SearchDocument\SearchDocument;
+
+class SearchCriteriaAnything implements SearchCriteria, \JsonSerializable
 {
+    private function __construct()
+    {
+    }
+
+    /**
+     * @return SearchCriteriaAnything
+     */
+    public static function create()
+    {
+        return new self();
+    }
+
+    /**
+     * @param SearchDocument $searchDocument
+     * @return bool
+     */
+    public function matches(SearchDocument $searchDocument)
+    {
+        return true;
+    }
+
+    /**
+     * @return string[]
+     */
+    public function jsonSerialize()
+    {
+        return [
+            'fieldName'  => '',
+            'fieldValue' => '',
+            'operation'  => 'Anything'
+        ];
+    }
 }
--- tests/Unit/Suites/DataPool/SearchEngine/SearchCriteria/SearchCriteriaAnythingTest.php
+++ tests/Unit/Suites/DataPool/SearchEngine/SearchCriteria/SearchCriteriaAnythingTest.php
@@ -2,6 +2,39 @@
 
 namespace LizardsAndPumpkins\DataPool\SearchEngine\SearchCriteria;
 
-class SearchCriteriaAnythingTest
+use LizardsAndPumpkins\DataPool\SearchEngine\SearchDocument\SearchDocument;
+
+/**
+ * @covers \LizardsAndPumpkins\DataPool\SearchEngine\SearchCriteria\SearchCriteriaAnything
+ */
+class SearchCriteriaAnythingTest extends \PHPUnit_Framework_TestCase
 {
+    public function testItImplementsTheSearchCriteriaInterface()
+    {
+        $this->assertInstanceOf(SearchCriteria::class, SearchCriteriaAnything::create());
+    }
+
+    public function testItMatchesAnySearchDocument()
+    {
+        $mockSearchDocument = $this->getMock(SearchDocument::class, [], [], '', false);
+        $this->assertTrue(SearchCriteriaAnything::create()->matches($mockSearchDocument));
+    }
+
+    public function testItImplementsJsonSerializable()
+    {
+        $this->assertInstanceOf(\JsonSerializable::class, SearchCriteriaAnything::create());
+    }
+
+    public function testItReturnsAnArrayRepresentationWhenJsonSerialized()
+    {
+        $result = SearchCriteriaAnything::create()->jsonSerialize();
+        
+        $expectation = [
+            'fieldName'  => '',
+            'fieldValue' => '',
+            'operation'  => 'Anything'
+        ];
+
+        $this->assertSame($expectation, $result);
+    }
 }
