b6aa8d90860c34ae7351f0094f4aaff391a0f551
Tim Bezhashvyly <tim.bezhashvyly@gmail.com>
2016-01-07 23:03:06 +0100

    Issue #657: Make ProductListingPageSnippetProjector implement Projector interface

--- src/Projection/Catalog/Import/Listing/ProductListingPageSnippetProjector.php
+++ src/Projection/Catalog/Import/Listing/ProductListingPageSnippetProjector.php
@@ -1,14 +1,15 @@
 <?php
 
-
 namespace LizardsAndPumpkins\Projection\Catalog\Import\Listing;
 
 use LizardsAndPumpkins\Context\Context;
 use LizardsAndPumpkins\Context\ContextSource;
 use LizardsAndPumpkins\DataPool\DataPoolWriter;
 use LizardsAndPumpkins\DataVersion;
+use LizardsAndPumpkins\Exception\InvalidProjectionSourceDataTypeException;
+use LizardsAndPumpkins\Projection\Projector;
 
-class ProductListingPageSnippetProjector
+class ProductListingPageSnippetProjector implements Projector
 {
     /**
      * @var DataPoolWriter
@@ -34,12 +35,19 @@ class ProductListingPageSnippetProjector
         $this->dataPoolWriter = $dataPoolWriter;
         $this->contextSource = $contextSource;
     }
-    
-    public function project(DataVersion $dataVersion)
+
+    /**
+     * @param mixed $projectionSourceData
+     */
+    public function project($projectionSourceData)
     {
+        if (!($projectionSourceData instanceof DataVersion)) {
+            throw new InvalidProjectionSourceDataTypeException('First argument must be instance of DataVersion.');
+        }
+
         @array_map(function (Context $context) {
             $snippets = $this->productListingPageSnippetRenderer->render($context);
             $this->dataPoolWriter->writeSnippets(...$snippets);
-        }, $this->contextSource->getAllAvailableContextsWithVersion($dataVersion));
+        }, $this->contextSource->getAllAvailableContextsWithVersion($projectionSourceData));
     }
 }
--- tests/Unit/Suites/Projection/Catalog/Import/Listing/ProductListingPageSnippetProjectorTest.php
+++ tests/Unit/Suites/Projection/Catalog/Import/Listing/ProductListingPageSnippetProjectorTest.php
@@ -7,6 +7,8 @@ use LizardsAndPumpkins\Context\Context;
 use LizardsAndPumpkins\Context\ContextSource;
 use LizardsAndPumpkins\DataPool\DataPoolWriter;
 use LizardsAndPumpkins\DataVersion;
+use LizardsAndPumpkins\Exception\InvalidProjectionSourceDataTypeException;
+use LizardsAndPumpkins\Projection\Projector;
 use LizardsAndPumpkins\Snippet;
 
 /**
@@ -18,22 +20,12 @@ class ProductListingPageSnippetProjectorTest extends \PHPUnit_Framework_TestCase
     /**
      * @var ProductListingPageSnippetProjector
      */
-    private $listingProjection;
+    private $projector;
 
     /**
      * @var DataPoolWriter|\PHPUnit_Framework_MockObject_MockObject
      */
     private $mockDataPoolWriter;
-
-    /**
-     * @var ContextSource|\PHPUnit_Framework_MockObject_MockObject
-     */
-    private $mockContextSource;
-
-    /**
-     * @var ProductListingPageSnippetRenderer|\PHPUnit_Framework_MockObject_MockObject
-     */
-    private $stubProductListingPageSnippetRenderer;
 
     /**
      * @var Snippet|\PHPUnit_Framework_MockObject_MockObject
@@ -43,29 +35,40 @@ class ProductListingPageSnippetProjectorTest extends \PHPUnit_Framework_TestCase
     protected function setUp()
     {
         $this->stubSnippet = $this->getMock(Snippet::class, [], [], '', false);
-        $this->stubProductListingPageSnippetRenderer = $this->getMockBuilder(ProductListingPageSnippetRenderer::class)
-            ->disableOriginalConstructor()
-            ->setMethods(['render'])
-            ->getMock();
-        $this->stubProductListingPageSnippetRenderer->method('render')->willReturn([$this->stubSnippet]);
+
+        /** @var ProductListingPageSnippetRenderer|\PHPUnit_Framework_MockObject_MockObject $stubSnippetRenderer */
+        $stubSnippetRenderer = $this->getMock(ProductListingPageSnippetRenderer::class, [], [], '', false);
+        $stubSnippetRenderer->method('render')->willReturn([$this->stubSnippet]);
         
         $this->mockDataPoolWriter = $this->getMock(DataPoolWriter::class, [], [], '', false);
-        
-        $this->mockContextSource = $this->getMock(ContextSource::class, [], [], '', false);
-        $this->mockContextSource->method('getAllAvailableContextsWithVersion')->willReturn(
-            [$this->getMock(Context::class)]
-        );
-        $this->listingProjection = new ProductListingPageSnippetProjector(
-            $this->stubProductListingPageSnippetRenderer,
+
+        /** @var ContextSource|\PHPUnit_Framework_MockObject_MockObject $stubContextSource */
+        $stubContextSource = $this->getMock(ContextSource::class, [], [], '', false);
+        $stubContextSource->method('getAllAvailableContextsWithVersion')->willReturn([$this->getMock(Context::class)]);
+
+        $this->projector = new ProductListingPageSnippetProjector(
+            $stubSnippetRenderer,
             $this->mockDataPoolWriter,
-            $this->mockContextSource
+            $stubContextSource
         );
+    }
+
+    public function testProjectorInterfaceIsImplemented()
+    {
+        $this->assertInstanceOf(Projector::class, $this->projector);
+    }
+
+    public function testExceptionIsThrownIfProjectionSourceDataIsNotAnInstanceOfDataVersion()
+    {
+        $invalidProjectionSourceData = new \stdClass();
+        $this->setExpectedException(InvalidProjectionSourceDataTypeException::class);
+        $this->projector->project($invalidProjectionSourceData);
     }
 
     public function testSnippetsAreWrittenToKeyValueStore()
     {
         $testVersion = DataVersion::fromVersionString('abc123');
         $this->mockDataPoolWriter->expects($this->once())->method('writeSnippets')->with($this->stubSnippet);
-        $this->listingProjection->project($testVersion);
+        $this->projector->project($testVersion);
     }
 }
