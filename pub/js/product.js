f0f810540d45a8ba7f149e7b1a587bd5772fe17d
Tim Bezhashvyly <tim.bezhashvyly@gmail.com>
2016-01-19 17:42:40 +0100

    Issue #677: Refactor product.js not to expose internal data

--- pub/js/product.js
+++ pub/js/product.js
@@ -1,54 +1,53 @@
 define(function () {
+    return function (productSourceData) {
+        var product = productSourceData;
 
-    function isDate(dateString) {
-        return dateString.match(/^\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}$/);
-    }
-
-    function getRawPrice(product) {
-        return parseInt(product.product['attributes']['raw_price']);
-    }
-
-    function getRawSpecialPrice(product) {
-        return parseInt(product.product['attributes']['raw_special_price']);
-    }
-
-    function getFinalRawPrice(product) {
-        if (product.hasSpecialPrice()) {
-            return getRawSpecialPrice(product);
+        function isDate(dateString) {
+            return dateString.match(/^\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}$/);
         }
 
-        return getRawPrice(product);
-    }
+        function getRawPrice() {
+            return parseInt(product['attributes']['raw_price']);
+        }
 
-    return function (productSourceData) {
-        this.product = productSourceData;
+        function getRawSpecialPrice() {
+            return parseInt(product['attributes']['raw_special_price']);
+        }
+
+        function getFinalRawPrice(product) {
+            if (product.hasSpecialPrice()) {
+                return getRawSpecialPrice();
+            }
+
+            return getRawPrice();
+        }
 
         this.getSku = function () {
-            return this.product['product_id'];
+            return product['product_id'];
         };
 
         this.getName = function () {
-            return this.product['attributes']['name'];
+            return product['attributes']['name'];
         };
 
         this.getUrlKey = function () {
-            return this.product['attributes']['url_key'];
+            return product['attributes']['url_key'];
         };
 
         this.getBrand = function () {
-            return this.product['attributes']['brand'];
+            return product['attributes']['brand'];
         };
 
         this.getGender = function () {
-            return this.product['attributes']['gender'];
+            return product['attributes']['gender'];
         };
 
         this.getMainImage = function () {
-            return this.product['images']['medium'][0];
+            return product['images']['medium'][0];
         };
 
         this.hasSpecialPrice = function () {
-            if (!this.product['attributes'].hasOwnProperty('raw_special_price')) {
+            if (!product['attributes'].hasOwnProperty('raw_special_price')) {
                 return false;
             }
 
@@ -56,57 +55,57 @@ define(function () {
         };
 
         this.getPrice = function () {
-            return this.product['attributes']['price'];
+            return product['attributes']['price'];
         };
 
         this.getSpecialPrice = function () {
-            return this.product['attributes']['special_price'];
+            return product['attributes']['special_price'];
         };
 
         this.hasBasePrice = function () {
-            return this.product['attributes'].hasOwnProperty('base_price_amount') && this.getBasePriceAmount() > 0;
+            return product['attributes'].hasOwnProperty('base_price_amount') && this.getBasePriceAmount() > 0;
         };
 
         this.getBasePrice = function () {
-            var price = getFinalRawPrice(this) / this.product['attributes']['price_base_unit'],
+            var price = getFinalRawPrice(this) / product['attributes']['price_base_unit'],
                 basePrice = price * this.getBasePriceBaseAmount() / this.getBasePriceAmount();
 
             return Math.round(basePrice * 100) / 100;
         };
 
         this.getBasePriceBaseAmount = function () {
-            return parseFloat(this.product['attributes']['base_price_base_amount']);
+            return parseFloat(product['attributes']['base_price_base_amount']);
         };
 
         this.getBasePriceAmount = function () {
-            return parseFloat(this.product['attributes']['base_price_amount']);
+            return parseFloat(product['attributes']['base_price_amount']);
         };
 
         this.getBasePriceUnit = function () {
-            return this.product['attributes']['base_price_unit'];
+            return product['attributes']['base_price_unit'];
         };
 
         this.isNew = function () {
-            if ((!this.product['attributes'].hasOwnProperty('news_from_date') ||
-                 !isDate(this.product['attributes']['news_from_date'])) &&
-                (!this.product['attributes'].hasOwnProperty('news_to_date') ||
-                 !isDate(this.product['attributes']['news_to_date']))
+            if ((!product['attributes'].hasOwnProperty('news_from_date') ||
+                 !isDate(product['attributes']['news_from_date'])) &&
+                (!product['attributes'].hasOwnProperty('news_to_date') ||
+                 !isDate(product['attributes']['news_to_date']))
             ) {
                 return false;
             }
 
             var currentDate = new Date();
 
-            if (this.product['attributes'].hasOwnProperty('news_from_date')) {
-                var newsFromDate = new Date(this.product['attributes']['news_from_date'].replace(/\s/, 'T'));
+            if (product['attributes'].hasOwnProperty('news_from_date')) {
+                var newsFromDate = new Date(product['attributes']['news_from_date'].replace(/\s/, 'T'));
 
                 if (newsFromDate > currentDate) {
                     return false;
                 }
             }
 
-            if (this.product['attributes'].hasOwnProperty('news_to_date')) {
-                var newsToDate = new Date(this.product['attributes']['news_to_date'].replace(/\s/, 'T'));
+            if (product['attributes'].hasOwnProperty('news_to_date')) {
+                var newsToDate = new Date(product['attributes']['news_to_date'].replace(/\s/, 'T'));
 
                 if (newsToDate < currentDate) {
                     return false;
