3c785bb0a07df8658386f0d6b96d94c173a253bf
Tim Bezhashvyly <tim.bezhashvyly@gmail.com>
2016-01-30 11:35:21 +0100

    Issue #752: Rename file log path environment variable

--- src/TwentyOneRunFactory.php
+++ src/TwentyOneRunFactory.php
@@ -253,7 +253,7 @@ class TwentyOneRunFactory implements Factory
     {
         /** @var ConfigReader $configReader */
         $configReader = $this->getMasterFactory()->createConfigReader();
-        return $configReader->get('log_path');
+        return $configReader->get('file_log_path');
     }
 
     /**
