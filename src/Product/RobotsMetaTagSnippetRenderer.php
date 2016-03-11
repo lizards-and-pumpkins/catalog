bae6f1a5dbad1338572a8e729c4d1957ab7aed35
Vinai Kopp <vinai@netzarbeiter.com>
2016-03-11 11:08:00 +0100

    Issue #794: Implement robots meta tag snippet renderer

---
+++ src/Product/RobotsMetaTagSnippetRenderer.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace LizardsAndPumpkins\Product;
+
+use LizardsAndPumpkins\Context\Context;
+use LizardsAndPumpkins\Projection\Catalog\ProductView;
+use LizardsAndPumpkins\Snippet;
+use LizardsAndPumpkins\SnippetKeyGenerator;
+use LizardsAndPumpkins\SnippetRenderer;
+
+class RobotsMetaTagSnippetRenderer implements SnippetRenderer
+{
+    /**
+     * @var SnippetKeyGenerator
+     */
+    private $keyGenerator;
+
+    private $robotsMetaTags = ['all', 'noindex'];
+
+    public function __construct(SnippetKeyGenerator $keyGenerator)
+    {
+        $this->keyGenerator = $keyGenerator;
+    }
+
+    /**
+     * @param ProductView $productView
+     * @return Snippet[]
+     */
+    public function render(ProductView $productView)
+    {
+        $context = $productView->getContext();
+        return array_map(function ($tagContent) use ($context) {
+            return $this->createRobotsMetaTagSnippetForContent($context, $tagContent);
+        }, $this->robotsMetaTags);
+    }
+
+    /**
+     * @param Context $context
+     * @param string $tagContent
+     * @return Snippet
+     */
+    private function createRobotsMetaTagSnippetForContent(Context $context, $tagContent)
+    {
+        $snippetKey = $this->keyGenerator->getKeyForContext($context, ['robots' => $tagContent]);
+        $snippetContent = sprintf('<meta name="robots" content="%s"/>', $tagContent);
+        return Snippet::create($snippetKey, $snippetContent);
+    }
+}
