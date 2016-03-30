08f6ed37344b0aedcc8cc4fc18c6076032d89782
Fabian Blechschmidt <blechschmidt@fabian-blechschmidt.de>
2016-03-30 16:54:50 +0200

    Issue #829: Add InMemory Implementations

---
+++ tests/Unit/Suites/DataPool/KeyValueStore/InMemory/InMemoryKeyValueStoreTest.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace LizardsAndPumpkins\DataPool\KeyValue\InMemory;
+
+use LizardsAndPumpkins\DataPool\KeyValueStore\Exception\KeyNotFoundException;
+use LizardsAndPumpkins\Util\Storage\Clearable;
+
+/**
+ * @covers \LizardsAndPumpkins\DataPool\KeyValue\InMemory\InMemoryKeyValueStore
+ */
+class InMemoryKeyValueStoreTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var InMemoryKeyValueStore
+     */
+    private $store;
+
+    public function setUp()
+    {
+        $this->store = new InMemoryKeyValueStore;
+    }
+
+    public function testValueIsSetAndRetrieved()
+    {
+        $key = 'key';
+        $value = 'value';
+
+        $this->store->set($key, $value);
+        $this->assertEquals($value, $this->store->get($key));
+    }
+
+    public function testTrueIsReturnedOnlyAfterValueIsSet()
+    {
+        $key = 'key';
+        $value = 'value';
+
+        $this->assertFalse($this->store->has($key));
+
+        $this->store->set($key, $value);
+        $this->assertTrue($this->store->has($key));
+    }
+
+    public function testExceptionIsThrownIfValueIsNotSet()
+    {
+        $this->setExpectedException(KeyNotFoundException::class);
+        $this->store->get('not set key');
+    }
+
+    public function testMultipleKeysAreSetAndRetrieved()
+    {
+        $keys = ['key1', 'key2'];
+        $values = ['foo', 'bar'];
+        $items = array_combine($keys, $values);
+
+        $this->store->multiSet($items);
+        $result = $this->store->multiGet($keys);
+
+        $this->assertSame($items, $result);
+    }
+
+    public function testClearableInterfaceIsImplemented()
+    {
+        $this->assertInstanceOf(Clearable::class, $this->store);
+    }
+
+    public function testStorageContentIsFlushed()
+    {
+        $key = 'key';
+        $value = 'value';
+
+        $this->store->set($key, $value);
+        $this->store->clear();
+        $this->assertFalse($this->store->has($key));
+    }
+}
