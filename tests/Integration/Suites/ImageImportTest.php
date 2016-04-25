1aac01d2220f716b17faf45e43194e6a192d14cc
Tim Bezhashvyly <tim.bezhashvyly@gmail.com>
2016-04-25 12:46:44 +0200

    Issue #867: Restore ImageImportTest

---
+++ tests/Integration/Suites/ImageImportTest.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace LizardsAndPumpkins;
+
+use LizardsAndPumpkins\Context\DataVersion\DataVersion;
+use LizardsAndPumpkins\Http\HttpHeaders;
+use LizardsAndPumpkins\Http\HttpRequest;
+use LizardsAndPumpkins\Http\HttpRequestBody;
+use LizardsAndPumpkins\Http\HttpsUrl;
+use LizardsAndPumpkins\Import\Image\ImageWasAddedDomainEvent;
+use LizardsAndPumpkins\Util\FileSystem\LocalFilesystem;
+
+class ImageImportTest extends AbstractIntegrationTest
+{
+    private function flushProcessedImagesDir()
+    {
+        $localFilesystem = new LocalFilesystem();
+        $processedImagesDir = sys_get_temp_dir() . '/' . IntegrationTestFactory::PROCESSED_IMAGES_DIR;
+        if (is_dir($processedImagesDir)) {
+            $localFilesystem->removeDirectoryAndItsContent($processedImagesDir);
+        }
+        mkdir($processedImagesDir, 0700, true);
+    }
+
+    protected function setUp()
+    {
+        if (!extension_loaded('imagick')) {
+            $this->markTestSkipped('The PHP extension imagick is not installed');
+        }
+
+        $this->flushProcessedImagesDir();
+    }
+
+    protected function tearDown()
+    {
+        $this->flushProcessedImagesDir();
+    }
+
+    public function testImagesAreImportedAndProcessed()
+    {
+        $request = HttpRequest::fromParameters(
+            HttpRequest::METHOD_GET,
+            HttpsUrl::fromString('http://example.com/'),
+            HttpHeaders::fromArray([]),
+            HttpRequestBody::fromString('')
+        );
+        $factory = $this->prepareIntegrationTestMasterFactoryForRequest($request);
+
+        $basePath = dirname(dirname(__DIR__));
+        $fixtureImageDirectory = $basePath . '/shared-fixture';
+        $absoluteImagePath = $fixtureImageDirectory . '/test_image.jpg';
+        $relativeImagePath = (new LocalFilesystem())
+            ->getRelativePath(getcwd(), $fixtureImageDirectory . '/test_image2.jpg');
+
+        $images = [$absoluteImagePath, $relativeImagePath];
+
+        $queue = $factory->getEventQueue();
+        foreach ($images as $imageFilePath) {
+            $queue->add(new ImageWasAddedDomainEvent($imageFilePath, DataVersion::fromVersionString('-1')));
+        }
+
+        $factory->createDomainEventConsumer()->process();
+
+        $logger = $factory->getLogger();
+        $this->failIfMessagesWhereLogged($logger);
+
+        foreach ($images as $originalImageFilePath) {
+            $imageName = basename($originalImageFilePath);
+            $processedImageDirectory = sys_get_temp_dir() . '/' . IntegrationTestFactory::PROCESSED_IMAGES_DIR;
+            $this->assertFileExists($processedImageDirectory . '/' . $imageName);
+
+            $fileInfo = getimagesize($processedImageDirectory . '/' . $imageName);
+            
+            $this->assertEquals('image/jpeg', $fileInfo['mime']);
+        }
+    }
+}
