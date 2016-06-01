a17a130dbf362d944c348530fbb8d36cff084070
Tim Bezhashvyly <tim.bezhashvyly@gmail.com>
2016-06-01 23:20:06 +0200

    Issue #895: Implement Locale value object

--- src/Context/Locale/Locale.php
+++ src/Context/Locale/Locale.php
@@ -2,7 +2,42 @@
 
 namespace LizardsAndPumpkins\Context\Locale;
 
+use LizardsAndPumpkins\Context\Locale\Exception\InvalidLocaleSpecificationException;
+
 class Locale
 {
     const CONTEXT_CODE = 'locale';
+
+    private $localeCode;
+
+    /**
+     * @param string $localeCode
+     */
+    public function __construct($localeCode)
+    {
+        $this->localeCode = $localeCode;
+    }
+
+    /**
+     * @param string $localeCode
+     * @return Locale
+     */
+    public static function fromCodeString($localeCode)
+    {
+        if (!is_string($localeCode)) {
+            throw new InvalidLocaleSpecificationException(
+                sprintf('The country specification has to be a string, got "%s".', gettype($localeCode))
+            );
+        }
+        
+        return new self($localeCode);
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString()
+    {
+        return $this->localeCode;
+    }
 }
