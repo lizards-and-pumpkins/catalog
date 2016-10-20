8b0a296bdf04ab05ad48490b82101aa9d9467da0
Tim Bezhashvyly <tim.bezhashvyly@gmail.com>
2016-10-20 19:43:50 +0200

    Issue: #760: Fix type-hint

--- tests/Unit/Suites/DataPool/KeyValueStore/SnippetTest.php
+++ tests/Unit/Suites/DataPool/KeyValueStore/SnippetTest.php
@@ -47,7 +47,7 @@ class SnippetTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
-     * @return string[]
+     * @return array[]
      */
     public function validKeyProvider() : array
     {
