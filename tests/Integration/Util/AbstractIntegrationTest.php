febb126691ceba21b4356c9e9cea6ee978b77658
Vinai Kopp <vinai@netzarbeiter.com>
2017-01-24 18:56:22 +0100

    Issue #986: Fix typo in exception message

--- tests/Integration/Util/AbstractIntegrationTest.php
+++ tests/Integration/Util/AbstractIntegrationTest.php
@@ -132,7 +132,7 @@ abstract class AbstractIntegrationTest extends \PHPUnit_Framework_TestCase
         $import = $factory->createCatalogImport();
         $dataVersion = DataVersion::fromVersionString('-1'); 
         if (count($fixtureCatalogFiles) === 0) {
-            throw new \RuntimeException('No catalog fixture fil specified.');
+            throw new \RuntimeException('No catalog fixture file specified.');
         }
         every($fixtureCatalogFiles, function(string $fixtureCatalogFile) use ($import, $dataVersion) {
             $import->importFile(__DIR__ . '/../../shared-fixture/' . $fixtureCatalogFile, $dataVersion);
