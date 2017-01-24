402e3f6d43c25279401e24e6e3a9b3d3de684e4e
Vinai Kopp <vinai@netzarbeiter.com>
2017-01-24 12:35:24 +0100

    Issue #983: Add more integration tests in regards to data versioning

--- tests/Integration/Suites/EdgeToEdgeImportCatalogTest.php
+++ tests/Integration/Suites/EdgeToEdgeImportCatalogTest.php
@@ -8,6 +8,7 @@ use LizardsAndPumpkins\Context\Country\Country;
 use LizardsAndPumpkins\DataPool\SearchEngine\Query\SortBy;
 use LizardsAndPumpkins\DataPool\SearchEngine\Query\SortDirection;
 use LizardsAndPumpkins\DataPool\SearchEngine\FacetFiltersToIncludeInResult;
+use LizardsAndPumpkins\Http\HttpResponse;
 use LizardsAndPumpkins\ProductSearch\QueryOptions;
 use LizardsAndPumpkins\DataPool\SearchEngine\SearchCriteria\SearchCriterionEqual;
 use LizardsAndPumpkins\Http\HttpHeaders;
@@ -24,6 +25,7 @@ use LizardsAndPumpkins\Http\HttpUrl;
 use LizardsAndPumpkins\Http\HttpRequest;
 use LizardsAndPumpkins\Import\XPathParser;
 use LizardsAndPumpkins\Util\Factory\SampleMasterFactory;
+use SebastianBergmann\Money\Currency;
 
 class EdgeToEdgeImportCatalogTest extends AbstractIntegrationTest
 {
@@ -32,14 +34,36 @@ class EdgeToEdgeImportCatalogTest extends AbstractIntegrationTest
      */
     private $factory;
 
-    private function importCatalog(string $importFileName)
+    private function importCatalogFixtureWithApiV1(string $importFileName)
+    {
+        $httpRequestBody = $this->buildV1ApiCatalogImportRequestBody($importFileName);
+        $this->importCatalogFixtureWithApi($httpRequestBody, 'v1');
+    }
+
+    private function importCatalogFixtureWithApiV2(string $importFileName, string $dataVersion)
+    {
+        $httpRequestBody = $this->buildV2ApiCatalogImportRequestBody($importFileName, $dataVersion);
+        $this->importCatalogFixtureWithApi($httpRequestBody, 'v2');
+    }
+
+    private function buildV1ApiCatalogImportRequestBody(string $importFileName): HttpRequestBody
+    {
+        $httpRequestBodyString = json_encode(['fileName' => $importFileName]);
+        return new HttpRequestBody($httpRequestBodyString);
+    }
+
+    private function buildV2ApiCatalogImportRequestBody(string $importFileName, string $dataVersion): HttpRequestBody
+    {
+        $httpRequestBodyString = json_encode(['fileName' => $importFileName, 'dataVersion' => $dataVersion]);
+        return new HttpRequestBody($httpRequestBodyString);
+    }
+
+    private function importCatalogFixtureWithApi(HttpRequestBody $httpRequestBody, $apiVersion)
     {
         $httpUrl = HttpUrl::fromString('http://example.com/api/catalog_import');
         $httpHeaders = HttpHeaders::fromArray([
-            'Accept' => 'application/vnd.lizards-and-pumpkins.catalog_import.v1+json'
+            'Accept' => 'application/vnd.lizards-and-pumpkins.catalog_import.' . $apiVersion . '+json'
         ]);
-        $httpRequestBodyString = json_encode(['fileName' => $importFileName]);
-        $httpRequestBody = new HttpRequestBody($httpRequestBodyString);
         $request = HttpRequest::fromParameters(HttpRequest::METHOD_PUT, $httpUrl, $httpHeaders, $httpRequestBody);
 
         $this->factory = $this->prepareIntegrationTestMasterFactoryForRequest($request);
@@ -51,13 +75,72 @@ class EdgeToEdgeImportCatalogTest extends AbstractIntegrationTest
         $this->processAllMessages($this->factory);
     }
 
-    public function testCatalogImportDomainEventPutsProductToKeyValueStoreAndSearchIndex()
+    private function getProductUrlKeyFromFixture(string $fixtureFile): string
+    {
+        $xml = file_get_contents(__DIR__ . '/../../shared-fixture/' . $fixtureFile);
+        return (new XPathParser($xml))->getXmlNodesArrayByXPath(
+            '//catalog/products/product/attributes/attribute[@name="non_canonical_url_key" and @locale="fr_FR"]'
+        )[0]['value'];
+    }
+
+    private function getProductNameFromFixture(string $fixtureFile): string
+    {
+        $xml = file_get_contents(__DIR__ . '/../../shared-fixture/' . $fixtureFile);
+        return (new XPathParser($xml))->getXmlNodesArrayByXPath(
+            '//catalog/products/product/attributes/attribute[@name="name" and @locale="fr_FR"]'
+        )[0]['value'];
+    }
+
+    private function getProductPriceFromFixture(string $fixtureFile): string
+    {
+        $xml = file_get_contents(__DIR__ . '/../../shared-fixture/' . $fixtureFile);
+        return (new XPathParser($xml))->getXmlNodesArrayByXPath(
+            '//catalog/products/product/attributes/attribute[@name="price" and @website="fr"]'
+        )[0]['value'];
+    }
+
+    private function getHttpRequestResponse(string $urlKey): HttpResponse
+    {
+        $httpUrl = HttpUrl::fromString('http://example.com/' . $urlKey);
+        $httpHeaders = HttpHeaders::fromArray([]);
+        $httpRequestBody = new HttpRequestBody('');
+        $request = HttpRequest::fromParameters(HttpRequest::METHOD_GET, $httpUrl, $httpHeaders, $httpRequestBody);
+        $this->factory = $this->prepareIntegrationTestMasterFactoryForRequest($request);
+        $implementationSpecificFactory = $this->getIntegrationTestFactory($this->factory);
+
+        $website = new InjectableDefaultWebFront($request, $this->factory, $implementationSpecificFactory);
+
+        return $website->processRequest();
+    }
+    
+    public function assertProductCanBeAccessedOnFrontend(string $urlKey, string $name, string $dataVersion)
+    {
+        $this->factory->createDataPoolWriter()->setCurrentDataVersion($dataVersion);
+        $response = $this->getHttpRequestResponse($urlKey);
+        if (! $response->getStatusCode() === HttpResponse::STATUS_OK) {
+            $this->fail(sprintf('Product "%s" not accessible on route "%s"', $name, $urlKey));
+        }
+        $this->assertContains($name, $response->getBody());
+    }
+    
+    public function assertProductPriceOnFrontend(string $urlKey, string $dataVersion, string $expectedPrice)
+    {
+        $price = Price::fromDecimalValue($expectedPrice)->round((new Currency('EUR'))->getDefaultFractionDigits());
+        $this->factory->createDataPoolWriter()->setCurrentDataVersion($dataVersion);
+        $response = $this->getHttpRequestResponse($urlKey);
+        if (! $response->getStatusCode() === HttpResponse::STATUS_OK) {
+            $this->fail(sprintf('Product not accessible on route "%s"', $urlKey));
+        }
+        $this->assertContains((string) $price->getAmount(), $response->getBody());
+    }
+
+    public function testCatalogImportApiPutsProductIntoKeyValueStoreAndSearchIndex()
     {
         $productId = new ProductId('118235-251');
         $productName = 'LED Arm-Signallampe';
         $expectedProductPrice = Price::fromDecimalValue(11.45)->getAmount();
 
-        $this->importCatalog('catalog.xml');
+        $this->importCatalogFixtureWithApiV1('catalog.xml');
 
         $logger = $this->factory->getLogger();
         $this->failIfMessagesWhereLogged($logger);
@@ -135,46 +218,28 @@ class EdgeToEdgeImportCatalogTest extends AbstractIntegrationTest
 
     public function testImportedProductIsAccessibleFromTheFrontend()
     {
-        $this->importCatalog('catalog.xml');
-
-        $xml = file_get_contents(__DIR__ . '/../../shared-fixture/catalog.xml');
-        $urlKeys = (new XPathParser($xml))->getXmlNodesArrayByXPath(
-            '//catalog/products/product/attributes/attribute[@name="url_key" and @locale="fr_FR"]'
-        );
-
-        $httpUrl = HttpUrl::fromString('http://example.com/' . $urlKeys[0]['value']);
-        $httpHeaders = HttpHeaders::fromArray([]);
-        $httpRequestBody = new HttpRequestBody('');
-        $request = HttpRequest::fromParameters(HttpRequest::METHOD_GET, $httpUrl, $httpHeaders, $httpRequestBody);
-        $this->prepareIntegrationTestMasterFactoryForRequest($request);
-        $implementationSpecificFactory = $this->getIntegrationTestFactory($this->factory);
+        $fixtureFile = 'catalog.xml';
+        $this->importCatalogFixtureWithApiV1($fixtureFile);
         
-        $website = new InjectableDefaultWebFront($request, $this->factory, $implementationSpecificFactory);
-        $response = $website->processRequest();
-
-        $this->assertContains('<body>', $response->getBody());
+        $this->assertProductCanBeAccessedOnFrontend(
+            $this->getProductUrlKeyFromFixture($fixtureFile),
+            $this->getProductNameFromFixture($fixtureFile),
+            $dataVersion = '-1'
+        );
     }
 
     public function testImportedProductIsAccessibleViaNonCanonicalUrlFromTheFrontend()
     {
-        $this->importCatalog('catalog.xml');
+        $fixtureFile = 'catalog.xml';
+        $this->importCatalogFixtureWithApiV1($fixtureFile);
 
-        $xml = file_get_contents(__DIR__ . '/../../shared-fixture/catalog.xml');
-        $urlKeys = (new XPathParser($xml))->getXmlNodesArrayByXPath(
+        $xml = file_get_contents(__DIR__ . '/../../shared-fixture/' . $fixtureFile);
+        $urlKey = (new XPathParser($xml))->getXmlNodesArrayByXPath(
             '//catalog/products/product/attributes/attribute[@name="non_canonical_url_key" and @locale="fr_FR"]'
-        );
+        )[0]['value'];
 
-        $httpUrl = HttpUrl::fromString('http://example.com/' . $urlKeys[0]['value']);
-        $httpHeaders = HttpHeaders::fromArray([]);
-        $httpRequestBody = new HttpRequestBody('');
-        $request = HttpRequest::fromParameters(HttpRequest::METHOD_GET, $httpUrl, $httpHeaders, $httpRequestBody);
-        $this->prepareIntegrationTestMasterFactoryForRequest($request);
-        $implementationSpecificFactory = $this->getIntegrationTestFactory($this->factory);
-
-        $website = new InjectableDefaultWebFront($request, $this->factory, $implementationSpecificFactory);
-        $response = $website->processRequest();
-
-        $this->assertContains('<body>', $response->getBody());
+        $name = $this->getProductNameFromFixture($fixtureFile);
+        $this->assertProductCanBeAccessedOnFrontend($urlKey, $name, $dataVersion = '-1');
     }
 
     public function testHttpResourceNotFoundResponseIsReturned()
@@ -192,10 +257,10 @@ class EdgeToEdgeImportCatalogTest extends AbstractIntegrationTest
         $response = $website->processRequest();
         $this->assertInstanceOf(HttpResourceNotFoundResponse::class, $response);
     }
-    
+
     public function testProductsWithValidDataAreImportedAndInvalidDataAreNotImportedButLogged()
     {
-        $this->importCatalog('catalog-with-invalid-product.xml');
+        $this->importCatalogFixtureWithApiV1('catalog-with-invalid-product.xml');
 
         $contextSource = $this->factory->createContextSource();
         $context = $contextSource->getAllAvailableContexts()[0];
@@ -246,5 +311,46 @@ class EdgeToEdgeImportCatalogTest extends AbstractIntegrationTest
                 }
             }, $messages);
         }
+    }
+
+    public function testProductImportedWithDifferentDataVersionsAreBothAccessibleFromFrontend()
+    {
+        $fixtureFileV1 = 'simple_product-v1.xml';
+        $fixtureFileV2 = 'simple_product-v2.xml';
+        $dataVersion1 = 'data-version-1';
+        $dataVersion2 = 'data-version-2';
+        
+        $this->importCatalogFixtureWithApiV2($fixtureFileV1, $dataVersion1);
+        $this->importCatalogFixtureWithApiV2($fixtureFileV2, $dataVersion2);
+
+        $name1 = $this->getProductNameFromFixture($fixtureFileV1);
+        $name2 = $this->getProductNameFromFixture($fixtureFileV2);
+        $urlKey1 = $this->getProductUrlKeyFromFixture($fixtureFileV1);
+        $urlKey2 = $this->getProductUrlKeyFromFixture($fixtureFileV2);
+
+        $this->factory->createDataPoolWriter()->setCurrentDataVersion('-1');
+        $response = $this->getHttpRequestResponse($urlKey1);
+        $this->assertSame(HttpResponse::STATUS_NOT_FOUND, $response->getStatusCode());
+        
+        $this->assertProductCanBeAccessedOnFrontend($urlKey1, $name1, $dataVersion1);
+        $this->assertProductCanBeAccessedOnFrontend($urlKey2, $name2, $dataVersion2);
+    }
+
+    public function testProductPriceIsProjectedWithoutDataVersion()
+    {
+        $fixtureFileV1 = 'simple_product-v1.xml';
+        $fixtureFileV2 = 'simple_product-v2.xml';
+        $dataVersion1 = 'data-version-1';
+        $dataVersion2 = 'data-version-2';
+
+        $this->importCatalogFixtureWithApiV2($fixtureFileV1, $dataVersion1);
+        $this->importCatalogFixtureWithApiV2($fixtureFileV2, $dataVersion2);
+
+        $urlKey1 = $this->getProductUrlKeyFromFixture($fixtureFileV1);
+        $urlKey2 = $this->getProductUrlKeyFromFixture($fixtureFileV2);
+        $expectedPrice = $this->getProductPriceFromFixture($fixtureFileV2);
+        
+        $this->assertProductPriceOnFrontend($urlKey1, $dataVersion1, $expectedPrice);
+        $this->assertProductPriceOnFrontend($urlKey2, $dataVersion2, $expectedPrice);
     }
 }
