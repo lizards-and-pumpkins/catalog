eb423b2d961a41190fbd4dd7a81a60d5fade3664
Vinai Kopp <vinai@netzarbeiter.com>
2017-01-25 12:26:14 +0100

    Issue #988: Add integration test for /api/current_version endpoint

---
+++ tests/Integration/Suites/CurrentVersionApiTest.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace LizardsAndPumpkins;
+
+use LizardsAndPumpkins\Http\HttpHeaders;
+use LizardsAndPumpkins\Http\HttpRequest;
+use LizardsAndPumpkins\Http\HttpRequestBody;
+use LizardsAndPumpkins\Http\HttpUrl;
+
+class CurrentVersionApiTest extends AbstractIntegrationTest
+{
+    public function testReturnsDefaultCurrentVersionAndEmptyPreviousVersion()
+    {
+        $request = HttpRequest::fromParameters(
+            HttpRequest::METHOD_GET,
+            HttpUrl::fromString('https://example.com/api/current_version'),
+            HttpHeaders::fromArray([
+                'Accept' => 'application/vnd.lizards-and-pumpkins.current_version.v1+json',
+            ]),
+            new HttpRequestBody('')
+        );
+        $factory = $this->prepareIntegrationTestMasterFactoryForRequest($request);
+        $implementationSpecificFactory = $this->getIntegrationTestFactory($factory);
+        $website = new InjectableDefaultWebFront($request, $factory, $implementationSpecificFactory);
+        $response = $website->processRequest();
+        $body = json_decode($response->getBody(), true);
+        $this->assertInternalType('array', $body);
+        $this->assertArrayHasKey('data', $body);
+        $this->assertArrayHasKey('current_version', $body['data']);
+        $this->assertNotEmpty($body['data']['current_version']);
+        $this->assertSame('', $body['data']['previous_version']);
+    }
+}
