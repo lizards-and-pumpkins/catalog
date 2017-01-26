ab52a6760f060891f032913ae52bbec17293e0c5
Vinai Kopp <vinai@netzarbeiter.com>
2017-01-26 17:29:59 +0100

    Issue #911: Add UpdateTemplateCommandHandler

---
+++ src/Import/RootTemplate/UpdateTemplateCommandHandler.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace LizardsAndPumpkins\Import\RootTemplate;
+
+use LizardsAndPumpkins\Messaging\Command\CommandHandler;
+use LizardsAndPumpkins\Messaging\Event\DomainEventQueue;
+use LizardsAndPumpkins\Messaging\Queue\Message;
+
+class UpdateTemplateCommandHandler implements CommandHandler
+{
+    /**
+     * @var UpdateTemplateCommand
+     */
+    private $command;
+
+    /**
+     * @var DomainEventQueue
+     */
+    private $domainEventQueue;
+
+    public function __construct(Message $message, DomainEventQueue $domainEventQueue)
+    {
+        $this->command = UpdateTemplateCommand::fromMessage($message);
+        $this->domainEventQueue = $domainEventQueue;
+    }
+
+    public function process()
+    {
+        $templateId = $this->command->getTemplateId();
+        $templateContent = $this->command->getTemplateContent();
+        $this->domainEventQueue->add(new TemplateWasUpdatedDomainEvent($templateId, $templateContent));
+    }
+}
---
+++ tests/Unit/Suites/Import/RootTemplate/UpdateTemplateCommandHandlerTest.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace LizardsAndPumpkins\Import\RootTemplate;
+
+use LizardsAndPumpkins\Context\DataVersion\DataVersion;
+use LizardsAndPumpkins\Messaging\Command\CommandHandler;
+use LizardsAndPumpkins\Messaging\Event\DomainEventQueue;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \LizardsAndPumpkins\Import\RootTemplate\UpdateTemplateCommandHandler
+ * @uses   \LizardsAndPumpkins\Import\RootTemplate\UpdateTemplateCommand
+ * @uses   \LizardsAndPumpkins\Import\RootTemplate\TemplateWasUpdatedDomainEvent
+ * @uses   \LizardsAndPumpkins\Context\DataVersion\DataVersion
+ * @uses   \LizardsAndPumpkins\Messaging\Queue\Message
+ * @uses   \LizardsAndPumpkins\Messaging\Queue\MessageName
+ * @uses   \LizardsAndPumpkins\Messaging\Queue\MessagePayload
+ * @uses   \LizardsAndPumpkins\Messaging\Queue\MessageMetadata
+ */
+class UpdateTemplateCommandHandlerTest extends TestCase
+{
+    private $testTemplateId = 'test';
+
+    private $testContent = 'test';
+
+    private $testDataVersion;
+
+    /**
+     * @var DomainEventQueue|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private $mockEventQueue;
+
+    protected function setUp()
+    {
+        $this->mockEventQueue = $this->createMock(DomainEventQueue::class);
+        $this->testDataVersion = DataVersion::fromVersionString('test');
+    }
+
+    private function createCommandHandler(): UpdateTemplateCommandHandler
+    {
+        $command = new UpdateTemplateCommand($this->testTemplateId, $this->testContent, $this->testDataVersion);
+        $message = $command->toMessage();
+        return new UpdateTemplateCommandHandler($message, $this->mockEventQueue);
+    }
+
+    public function testIsACommandHandler()
+    {
+        $this->assertInstanceOf(CommandHandler::class, $this->createCommandHandler());
+    }
+
+    public function testEmitsATemplateWasUpdatedEvent()
+    {
+        $this->mockEventQueue->expects($this->once())->method('add')
+            ->willReturnCallback(function(TemplateWasUpdatedDomainEvent $event) {
+                $this->assertSame($this->testTemplateId, $event->getTemplateId());
+                $this->assertSame($this->testContent, $event->getTemplateContent());
+                // TODO: check for data version once it's implemented!
+            });
+        $this->createCommandHandler()->process();
+    }
+}
