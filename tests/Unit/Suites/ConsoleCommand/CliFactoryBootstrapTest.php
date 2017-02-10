17daa6ff1fb403dbac31ecfe1c688afaffbec54e
Vinai Kopp <vinai@netzarbeiter.com>
2017-02-10 16:09:01 +0000

    Issue #998: Add @uses annotations

--- tests/Unit/Suites/ConsoleCommand/CliFactoryBootstrapTest.php
+++ tests/Unit/Suites/ConsoleCommand/CliFactoryBootstrapTest.php
@@ -16,14 +16,82 @@ use PHPUnit\Framework\TestCase;
 
 /**
  * @covers \LizardsAndPumpkins\ConsoleCommand\CliFactoryBootstrap
+ * @uses   \LizardsAndPumpkins\Context\DataVersion\ContextVersion
+ * @uses   \LizardsAndPumpkins\Context\DataVersion\DataVersion
+ * @uses   \LizardsAndPumpkins\Context\SelfContainedContextBuilder
+ * @uses   \LizardsAndPumpkins\DataPool\DataPoolReader
+ * @uses   \LizardsAndPumpkins\DataPool\DataPoolWriter
+ * @uses   \LizardsAndPumpkins\DataPool\DataVersion\SetCurrentDataVersionCommandHandler
+ * @uses   \LizardsAndPumpkins\DataPool\KeyGenerator\CompositeSnippetKeyGeneratorLocatorStrategy
+ * @uses   \LizardsAndPumpkins\DataPool\KeyGenerator\GenericSnippetKeyGenerator
+ * @uses   \LizardsAndPumpkins\Import\CatalogImport
+ * @uses   \LizardsAndPumpkins\Import\CatalogImportWasTriggeredDomainEventHandler
+ * @uses   \LizardsAndPumpkins\Import\ContentBlock\ContentBlockProjector
+ * @uses   \LizardsAndPumpkins\Import\ContentBlock\ContentBlockSnippetKeyGeneratorLocatorStrategy
+ * @uses   \LizardsAndPumpkins\Import\ContentBlock\ContentBlockSnippetRenderer
+ * @uses   \LizardsAndPumpkins\Import\ContentBlock\ContentBlockWasUpdatedDomainEventHandler
+ * @uses   \LizardsAndPumpkins\Import\ContentBlock\UpdateContentBlockCommandHandler
+ * @uses   \LizardsAndPumpkins\Import\Image\AddImageCommandHandler
+ * @uses   \LizardsAndPumpkins\Import\Image\ImageWasAddedDomainEventHandler
+ * @uses   \LizardsAndPumpkins\Import\ImportCatalogCommandHandler
+ * @uses   \LizardsAndPumpkins\Import\Price\PriceSnippetRenderer
+ * @uses   \LizardsAndPumpkins\Import\Product\AttributeCode
+ * @uses   \LizardsAndPumpkins\Import\Product\ConfigurableProductXmlToProductBuilder
+ * @uses   \LizardsAndPumpkins\Import\Product\Image\ProductImageImportCommandLocator
+ * @uses   \LizardsAndPumpkins\Import\Product\Listing\ProductListingImportCommandLocator
+ * @uses   \LizardsAndPumpkins\Import\Product\ProductImportCommandLocator
+ * @uses   \LizardsAndPumpkins\Import\Product\ProductJsonSnippetRenderer
+ * @uses   \LizardsAndPumpkins\Import\Product\ProductProjector
+ * @uses   \LizardsAndPumpkins\Import\Product\ProductWasUpdatedDomainEventHandler
+ * @uses   \LizardsAndPumpkins\Import\Product\ProductXmlToProductBuilderLocator
+ * @uses   \LizardsAndPumpkins\Import\Product\QueueImportCommands
+ * @uses   \LizardsAndPumpkins\Import\Product\RobotsMetaTagSnippetRenderer
+ * @uses   \LizardsAndPumpkins\Import\Product\UpdateProductCommandHandler
+ * @uses   \LizardsAndPumpkins\Import\Product\UrlKey\UrlKeyForContextCollector
+ * @uses   \LizardsAndPumpkins\Import\RootTemplate\Import\TemplateProjectorLocator
+ * @uses   \LizardsAndPumpkins\Import\RootTemplate\TemplateWasUpdatedDomainEventHandler
+ * @uses   \LizardsAndPumpkins\Import\RootTemplate\UpdateTemplateCommandHandler
+ * @uses   \LizardsAndPumpkins\Import\SnippetRendererCollection
+ * @uses   \LizardsAndPumpkins\Import\TemplateRendering\BlockRenderer
+ * @uses   \LizardsAndPumpkins\Logging\LoggingCommandHandlerFactory
+ * @uses   \LizardsAndPumpkins\Logging\LoggingDomainEventHandlerFactory
+ * @uses   \LizardsAndPumpkins\Logging\LoggingQueueDecorator
+ * @uses   \LizardsAndPumpkins\Logging\LoggingQueueFactory
+ * @uses   \LizardsAndPumpkins\Logging\ProcessTimeLoggingCommandHandlerDecorator
+ * @uses   \LizardsAndPumpkins\Logging\ProcessTimeLoggingDomainEventHandlerDecorator
+ * @uses   \LizardsAndPumpkins\Messaging\Consumer\ShutdownWorkerDirectiveHandler
+ * @uses   \LizardsAndPumpkins\Messaging\Queue\EnqueuesMessageEnvelope
+ * @uses   \LizardsAndPumpkins\ProductDetail\Import\ConfigurableProductJsonSnippetRenderer
+ * @uses   \LizardsAndPumpkins\ProductDetail\ProductCanonicalTagSnippetRenderer
+ * @uses   \LizardsAndPumpkins\ProductDetail\ProductDetailPageRobotsMetaTagSnippetRenderer
+ * @uses   \LizardsAndPumpkins\ProductDetail\ProductDetailViewSnippetRenderer
+ * @uses   \LizardsAndPumpkins\ProductListing\AddProductListingCommandHandler
+ * @uses   \LizardsAndPumpkins\ProductListing\Import\ProductListingContentBlockSnippetKeyGeneratorLocatorStrategy
+ * @uses   \LizardsAndPumpkins\ProductListing\Import\ProductListingDescriptionSnippetRenderer
+ * @uses   \LizardsAndPumpkins\ProductListing\Import\ProductListingRobotsMetaTagSnippetRenderer
+ * @uses   \LizardsAndPumpkins\ProductListing\Import\ProductListingSnippetProjector
+ * @uses   \LizardsAndPumpkins\ProductListing\Import\ProductListingSnippetRenderer
+ * @uses   \LizardsAndPumpkins\ProductListing\Import\ProductListingTemplateProjector
+ * @uses   \LizardsAndPumpkins\ProductListing\Import\ProductListingTemplateSnippetRenderer
+ * @uses   \LizardsAndPumpkins\ProductListing\Import\ProductListingTitleSnippetRenderer
+ * @uses   \LizardsAndPumpkins\ProductListing\Import\ProductSearchResultMetaSnippetRenderer
+ * @uses   \LizardsAndPumpkins\ProductListing\ProductInListingSnippetRenderer
+ * @uses   \LizardsAndPumpkins\ProductListing\ProductListingCanonicalTagSnippetRenderer
+ * @uses   \LizardsAndPumpkins\ProductListing\ProductListingWasAddedDomainEventHandler
+ * @uses   \LizardsAndPumpkins\ProductSearch\Import\AttributeValueCollectorLocator
+ * @uses   \LizardsAndPumpkins\ProductSearch\Import\ProductSearchDocumentBuilder
+ * @uses   \LizardsAndPumpkins\Translation\TranslatorRegistry
+ * @uses   \LizardsAndPumpkins\Util\Factory\CommonFactory
  * @uses   \LizardsAndPumpkins\Util\Factory\FactoryTrait
  * @uses   \LizardsAndPumpkins\Util\Factory\MasterFactoryTrait
+ * @uses   \LizardsAndPumpkins\Util\SnippetCodeValidator
  */
 class CliFactoryBootstrapTest extends TestCase
 {
     private function createSpyFactory(): Factory
     {
-        return new class implements Factory, FactoryWithCallback {
+        return new class implements Factory, FactoryWithCallback
+        {
             use FactoryTrait;
 
             public $wasRegistered = false;
@@ -59,7 +127,7 @@ class CliFactoryBootstrapTest extends TestCase
             }
         };
         $spyCommonFactory->resetRegistrationCount();
-        
+
         return $spyCommonFactory;
     }
 
@@ -68,7 +136,7 @@ class CliFactoryBootstrapTest extends TestCase
         return new class extends CliFactoryBootstrap
         {
             private static $originalCommonFactory;
-            
+
             public function setCommonFactoryClass(string $commonFactoryClass)
             {
                 if (is_null(static::$originalCommonFactory)) {
@@ -76,16 +144,16 @@ class CliFactoryBootstrapTest extends TestCase
                 }
                 static::$commonFactoryClass = $commonFactoryClass;
             }
-            
+
             public function __destruct()
             {
-                if (! is_null(static::$originalCommonFactory)) {
+                if (!is_null(static::$originalCommonFactory)) {
                     static::$commonFactoryClass = static::$originalCommonFactory;
                 }
             }
         };
     }
-    
+
     public function testReturnsMasterFactoryInstance()
     {
         $this->assertInstanceOf(MasterFactory::class, (CliFactoryBootstrap::createMasterFactory()));
@@ -103,7 +171,7 @@ class CliFactoryBootstrapTest extends TestCase
     public function testRegistersCommonFactoryWithoutItBeingSpecified()
     {
         $testCliBootstrap = $this->createTestCliFactoryBootstrap();
-        
+
         $spyCommonFactory = $this->createSpyCommonFactory();
         $testCliBootstrap->setCommonFactoryClass(get_class($spyCommonFactory));
 
@@ -118,9 +186,9 @@ class CliFactoryBootstrapTest extends TestCase
 
         $spyCommonFactory = $this->createSpyCommonFactory();
         $testCliBootstrap->setCommonFactoryClass(get_class($spyCommonFactory));
-        
+
         $testCliBootstrap->createMasterFactory($spyCommonFactory);
-        
+
         $this->assertSame(1, $spyCommonFactory->getRegistrationCount());
     }
 
@@ -133,11 +201,11 @@ class CliFactoryBootstrapTest extends TestCase
     public function testLoggingFactoriesAreRegistered()
     {
         $factory = CliFactoryBootstrap::createLoggingMasterFactory(new UnitTestFactory($this));
-        
+
         $queue = $factory->createEventMessageQueue();
         $commandHandler = $factory->createUpdateContentBlockCommandHandler();
         $eventHandler = $factory->createTemplateWasUpdatedDomainEventHandler();
-        
+
         $this->assertInstanceOf(LoggingQueueDecorator::class, $queue);
         $this->assertInstanceOf(ProcessTimeLoggingCommandHandlerDecorator::class, $commandHandler);
         $this->assertInstanceOf(ProcessTimeLoggingDomainEventHandlerDecorator::class, $eventHandler);
