cc0fb1e7822bebc294f12800f0f37c56272e9c8d
Vinai Kopp <vinai@netzarbeiter.com>
2017-02-20 18:50:07 +0100

    Issue #998: Migrate command consumer console command from sample project

---
+++ tests/Unit/Suites/ConsoleCommand/Command/ConsumeCommandsConsoleCommandTest.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace LizardsAndPumpkins\ConsoleCommand\Command;
+
+use LizardsAndPumpkins\ConsoleCommand\ConsoleCommand;
+use LizardsAndPumpkins\Messaging\Command\CommandConsumer;
+use LizardsAndPumpkins\Util\Factory\MasterFactory;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \LizardsAndPumpkins\ConsoleCommand\Command\ConsumeCommandsConsoleCommand
+ */
+class ConsumeCommandsConsoleCommandTest extends TestCase
+{
+    /**
+     * @var MasterFactory|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private $stubMasterFactory;
+
+    protected function setUp()
+    {
+        $this->stubMasterFactory = $this->getMockBuilder(MasterFactory::class)
+            ->setMethods(array_merge(get_class_methods(MasterFactory::class), ['createCommandConsumer']))
+            ->disableOriginalConstructor()
+            ->getMock();
+    }
+    
+    public function testIsAConsoleCommand()
+    {
+        $command = new ConsumeCommandsConsoleCommand($this->stubMasterFactory);
+        $this->assertInstanceOf(ConsoleCommand::class, $command);
+    }
+
+    public function testCallsProcessOnTheCommandConsumer()
+    {
+        $mockCommandConsumer = $this->createMock(CommandConsumer::class);
+        $mockCommandConsumer->expects($this->once())->method('process');
+        $this->stubMasterFactory->method('createCommandConsumer')->willReturn($mockCommandConsumer);
+        (new ConsumeCommandsConsoleCommand($this->stubMasterFactory))->run();
+    }
+}
