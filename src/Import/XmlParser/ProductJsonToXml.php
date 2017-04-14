825b239df50d9455c8453b1e2e42e1701f1585ee
Fabian Blechschmidt <blechschmidt@fabian-blechschmidt.de>
2017-04-14 23:07:55 +0200

    Issue #1043: ProductJsonToXml

---
+++ src/Import/XmlParser/ProductJsonToXml.php
@@ -0,0 +1,66 @@
+<?php
+declare(strict_types=1);
+
+namespace LizardsAndPumpkins\Import\XmlParser;
+
+class ProductJsonToXml
+{
+    /**
+     * @var \XMLWriter
+     */
+    private $writer;
+    /**
+     * @var string[]
+     */
+    private $productNodeAttributes = ['type', 'sku', 'tax_class'];
+
+    public function toXml(string $product): string
+    {
+        $product = json_decode($product, true);
+        $this->startDocument();
+
+        $this->writeProducts($product);
+
+        $this->writer->endDocument();
+        return $this->writer->outputMemory();
+    }
+
+    private function startDocument()
+    {
+        $this->writer = new \XMLWriter();
+        $this->writer->openMemory();
+        $this->writer->startDocument('1.0', 'UTF-8');
+        $this->writeCatalogRoot();
+    }
+
+    private function writeCatalogRoot()
+    {
+        $this->writer->startElement('catalog');
+        $this->writer->writeAttribute('xmlns', 'http://lizardsandpumpkins.com');
+        $this->writer->writeAttribute('xmlns:xsi', 'http://www.w3.org/2001/XMLSchema-instance');
+        $this->writer->writeAttribute('xsi:schemaLocation', 'http://lizardsandpumpkins.com ../../schema/catalog.xsd');
+        $this->writer->endElement();
+    }
+
+    /**
+     * @param string[] $product
+     */
+    private function writeProducts(array $product)
+    {
+        $this->writer->startElement('products');
+        $this->writeProduct($product);
+        $this->writer->endElement();
+    }
+
+    /**
+     * @param string[] $product
+     */
+    private function writeProduct(array $product)
+    {
+        $this->writer->startElement('product');
+        foreach ($this->productNodeAttributes as $a) {
+            $this->writer->writeAttribute($a, $product[$a]);
+        }
+        $this->writer->endElement();
+    }
+}
---
+++ tests/Unit/Suites/Import/XmlParser/ProductJsonToXmlTest.php
@@ -0,0 +1,61 @@
+<?php
+declare(strict_types=1);
+
+namespace LizardsAndPumpkins\Import\XmlParser;
+
+use PHPUnit\Framework\TestCase;
+
+class ProductJsonToXmlTest extends TestCase
+{
+    private $sku = '118235-251';
+    private $type = 'simple';
+    private $taxClass = '19%';
+    /**
+     * @var ProductJsonToXml
+     */
+    private $productJsonToXml;
+
+    public function testImplementsProductJsonToXml()
+    {
+        $this->assertInstanceOf(ProductJsonToXml::class, $this->productJsonToXml);
+    }
+
+    public function testStartsWithXmlHeader()
+    {
+        $xml = $this->productJsonToXml->toXml($this->getProductJson());
+        $this->assertContains('<?xml version="1.0" encoding="UTF-8"?>', $xml);
+    }
+
+    public function testContainsCatalogRoot()
+    {
+        $xml = $this->productJsonToXml->toXml($this->getProductJson());
+        $rootNode = '<catalog xmlns="http://lizardsandpumpkins.com" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://lizardsandpumpkins.com ../../schema/catalog.xsd"';
+
+        $this->assertContains($rootNode, $xml);
+    }
+
+    public function testWritesProductNodeWithAttributes()
+    {
+        $xml = $this->productJsonToXml->toXml($this->getProductJson());
+        $productNode = "<product type=\"{$this->type}\" sku=\"{$this->sku}\" tax_class=\"{$this->taxClass}\"";
+
+        $this->assertContains($productNode, $xml);
+    }
+
+    protected function setUp()
+    {
+        $this->productJsonToXml = new ProductJsonToXml();
+    }
+
+    /**
+     * @return string
+     */
+    private function getProductJson(): string
+    {
+        return json_encode([
+            'sku' => $this->sku,
+            'type' => $this->type,
+            'tax_class' => $this->taxClass,
+        ]);
+    }
+}
