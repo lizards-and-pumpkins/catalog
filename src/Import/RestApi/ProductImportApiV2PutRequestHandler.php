e7f2df81ebea6ee3271ff72030a2ecaf8eb1f0cb
Fabian Blechschmidt <blechschmidt@fabian-blechschmidt.de>
2017-04-15 13:23:05 +0200

    Issue #1043: Implement ProductImportApiV2PutRequestHandler
    transform json from request to xml and pass it catalog import

--- src/Import/RestApi/ProductImportApiV2PutRequestHandler.php
+++ src/Import/RestApi/ProductImportApiV2PutRequestHandler.php
@@ -6,10 +6,27 @@ namespace LizardsAndPumpkins\Import\RestApi;
 use LizardsAndPumpkins\Http\ContentDelivery\GenericHttpResponse;
 use LizardsAndPumpkins\Http\HttpRequest;
 use LizardsAndPumpkins\Http\HttpResponse;
+use LizardsAndPumpkins\Import\CatalogImport;
+use LizardsAndPumpkins\Import\RestApi\Exception\CatalogImportProductDataNotFoundInRequestBodyException;
+use LizardsAndPumpkins\Import\XmlParser\ProductJsonToXml;
 use LizardsAndPumpkins\RestApi\ApiRequestHandler;
 
 class ProductImportApiV2PutRequestHandler extends ApiRequestHandler
 {
+    /**
+     * @var ProductJsonToXml
+     */
+    private $productJsonToXml;
+    /**
+     * @var CatalogImport
+     */
+    private $catalogImport;
+
+    public function __construct(ProductJsonToXml $productJsonToXml, CatalogImport $catalogImport)
+    {
+        $this->productJsonToXml = $productJsonToXml;
+        $this->catalogImport = $catalogImport;
+    }
 
     public function canProcess(HttpRequest $request): bool
     {
@@ -18,9 +35,30 @@ class ProductImportApiV2PutRequestHandler extends ApiRequestHandler
 
     public function processRequest(HttpRequest $request): HttpResponse
     {
+        $productData = $this->getProductDataFromRequest($request);
+        $productXml = $this->productJsonToXml->toXml($productData);
 
+        $this->catalogImport->addProductsAndProductImagesToQueue($productXml);
 
         return $this->getResponse($request);
+    }
+
+    private function getProductDataFromRequest(HttpRequest $request): string
+    {
+        $requestArguments = json_decode($request->getRawBody(), true);
+
+        if (!$this->hasArgument($requestArguments, 'productData')) {
+            throw new CatalogImportProductDataNotFoundInRequestBodyException(
+                'Product data is not found in request body.'
+            );
+        }
+
+        return $requestArguments['productData'];
+    }
+
+    private function hasArgument($requestArguments, string $argument): bool
+    {
+        return is_array($requestArguments) && isset($requestArguments[$argument]) && $requestArguments[$argument];
     }
 
     protected function getResponse(HttpRequest $request): HttpResponse
--- tests/Unit/Suites/Import/RestApi/ProductImportApiV2PutRequestHandlerTest.php
+++ tests/Unit/Suites/Import/RestApi/ProductImportApiV2PutRequestHandlerTest.php
@@ -5,16 +5,27 @@ namespace Import\RestApi;
 
 use LizardsAndPumpkins\Http\HttpRequest;
 use LizardsAndPumpkins\Http\HttpResponse;
+use LizardsAndPumpkins\Import\CatalogImport;
 use LizardsAndPumpkins\Import\RestApi\ProductImportApiV2PutRequestHandler;
+use LizardsAndPumpkins\Import\XmlParser\ProductJsonToXml;
 use LizardsAndPumpkins\RestApi\ApiRequestHandler;
 use PHPUnit\Framework\TestCase;
 
 class ProductImportApiV2PutRequestHandlerTest extends TestCase
 {
+    private $productJson = 'DATA';
     /**
      * @var ProductImportApiV2PutRequestHandler
      */
     private $handler;
+    /**
+     * @var ProductJsonToXml|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private $productJsonToXmlMock;
+    /**
+     * @var CatalogImport|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private $catalogImport;
 
     public function testIsProductImportRequestHandler()
     {
@@ -28,16 +39,26 @@ class ProductImportApiV2PutRequestHandlerTest extends TestCase
 
     public function testCanProcessPut()
     {
-        /** @var HttpRequest|\PHPUnit_Framework_MockObject_MockObject $request */
-        $request = $this->createMock(HttpRequest::class);
+        $request = $this->createValidRequestMock();
         $request->method('getMethod')->willReturn(HttpRequest::METHOD_PUT);
         $this->assertTrue($this->handler->canProcess($request));
     }
 
-    public function testCanNotProcessGet()
+    /**
+     * @return HttpRequest|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private function createValidRequestMock(): HttpRequest
     {
+        $productJson = json_encode(['productData' => $this->productJson]);
         /** @var HttpRequest|\PHPUnit_Framework_MockObject_MockObject $request */
         $request = $this->createMock(HttpRequest::class);
+        $request->method('getRawBody')->willReturn($productJson);
+        return $request;
+    }
+
+    public function testCanNotProcessGet()
+    {
+        $request = $this->createValidRequestMock();
         $request->method('getMethod')->willReturn(HttpRequest::METHOD_GET);
         $this->assertFalse($this->handler->canProcess($request));
     }
@@ -47,13 +68,12 @@ class ProductImportApiV2PutRequestHandlerTest extends TestCase
         $expectedCode = HttpResponse::STATUS_ACCEPTED;
         $expectedBody = '';
         $expectedHeaders = [
-            'Access-Control-Allow-Origin' => '*',
+            'Access-Control-Allow-Origin'  => '*',
             'Access-Control-Allow-Methods' => '*',
-            'Content-Type' => 'application/json',
+            'Content-Type'                 => 'application/json',
         ];
 
-        /** @var HttpRequest|\PHPUnit_Framework_MockObject_MockObject $request */
-        $request = $this->createMock(HttpRequest::class);
+        $request = $this->createValidRequestMock();
         $response = $this->handler->process($request);
 
         $this->assertEquals($expectedCode, $response->getStatusCode());
@@ -61,8 +81,44 @@ class ProductImportApiV2PutRequestHandlerTest extends TestCase
         $this->assertEquals($expectedHeaders, $response->getHeaders()->getAll());
     }
 
+    public function testReturnsBadRequestIfProductDataNotSet()
+    {
+        /** @var HttpRequest|\PHPUnit_Framework_MockObject_MockObject $request */
+        $request = $this->createMock(HttpRequest::class);
+
+        $response = $this->handler->process($request);
+
+        $this->assertEquals(HttpResponse::STATUS_BAD_REQUEST, $response->getStatusCode());
+        $this->assertJson($response->getBody());
+        $this->assertEquals(
+            'Product data is not found in request body.',
+            json_decode($response->getBody(), true)['error']
+        );
+    }
+
+    public function testSendJsonAndCallJsonToXmlWithIt()
+    {
+        $request = $this->createValidRequestMock();
+        $this->productJsonToXmlMock->expects($this->once())->method('toXml')->with($this->productJson);
+
+        $this->handler->process($request);
+    }
+
+    public function testSendXmlToCatalogImport()
+    {
+        $request = $this->createValidRequestMock();
+        $productXml = 'PRODUCT_XML';
+        $this->productJsonToXmlMock->expects($this->once())->method('toXml')->willReturn($productXml);
+
+        $this->catalogImport->expects($this->once())->method('addProductsAndProductImagesToQueue')->with($productXml);
+
+        $this->handler->process($request);
+    }
+
     protected function setUp()
     {
-        $this->handler = new ProductImportApiV2PutRequestHandler();
+        $this->productJsonToXmlMock = $this->createMock(ProductJsonToXml::class);
+        $this->catalogImport = $this->createMock(CatalogImport::class);
+        $this->handler = new ProductImportApiV2PutRequestHandler($this->productJsonToXmlMock, $this->catalogImport);
     }
 }
