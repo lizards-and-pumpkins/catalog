a15eeb92128628d4aabe3390cd1b1c8ea5d5373d
Vinai Kopp <vinai@netzarbeiter.com>
2017-05-10 19:52:20 +0200

    Issue #1048: Add ability to specify requested data version via dataVersion GET param

--- src/Context/DataVersion/ContextVersion.php
+++ src/Context/DataVersion/ContextVersion.php
@@ -4,10 +4,14 @@ declare(strict_types=1);
 
 namespace LizardsAndPumpkins\Context\DataVersion;
 
+use LizardsAndPumpkins\Context\ContextBuilder;
 use LizardsAndPumpkins\Context\ContextPartBuilder;
+use LizardsAndPumpkins\Http\HttpRequest;
 
 class ContextVersion implements ContextPartBuilder
 {
+    const DATA_VERSION_REQUEST_PARAM = 'dataVersion';
+
     /**
      * @var DataVersion
      */
@@ -22,13 +26,50 @@ class ContextVersion implements ContextPartBuilder
      * @param mixed[] $inputDataSet
      * @return string
      */
-    public function getValue(array $inputDataSet) : string
+    public function getValue(array $inputDataSet): string
     {
-        return (string) ($inputDataSet[DataVersion::CONTEXT_CODE] ?? $this->dataVersion);
+        if ($this->hasVersion($inputDataSet)) {
+            return (string) $this->getVersion($inputDataSet);
+        }
+        if ($this->hasVersionRequestParam($inputDataSet)) {
+            return (string) $this->getVersionRequestParamValue($inputDataSet);
+        }
+        return (string) $this->dataVersion;
     }
 
-    public function getCode() : string
+    public function getCode(): string
     {
         return DataVersion::CONTEXT_CODE;
+    }
+
+    private function hasVersion(array $inputDataSet): bool
+    {
+        return isset($inputDataSet[DataVersion::CONTEXT_CODE]);
+    }
+
+    private function getVersion(array $inputDataSet)
+    {
+        return $inputDataSet[DataVersion::CONTEXT_CODE];
+    }
+
+    private function hasRequest(array $inputDataSet): bool
+    {
+        return isset($inputDataSet[ContextBuilder::REQUEST]);
+    }
+
+    private function getRequest(array $inputDataSet): HttpRequest
+    {
+        return $inputDataSet[ContextBuilder::REQUEST];
+    }
+
+    private function hasVersionRequestParam(array $inputDataSet): bool
+    {
+        return $this->hasRequest($inputDataSet) &&
+               $this->getRequest($inputDataSet)->hasQueryParameter(self::DATA_VERSION_REQUEST_PARAM);
+    }
+
+    private function getVersionRequestParamValue(array $inputDataSet)
+    {
+        return $this->getRequest($inputDataSet)->getQueryParameter(self::DATA_VERSION_REQUEST_PARAM);
     }
 }
--- tests/Unit/Suites/Context/DataVersion/ContextVersionTest.php
+++ tests/Unit/Suites/Context/DataVersion/ContextVersionTest.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace LizardsAndPumpkins\Context\DataVersion;
 
+use LizardsAndPumpkins\Context\ContextBuilder;
 use LizardsAndPumpkins\Context\ContextPartBuilder;
+use LizardsAndPumpkins\Http\HttpRequest;
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -12,40 +14,68 @@ use PHPUnit\Framework\TestCase;
  */
 class ContextVersionTest extends TestCase
 {
-    private $testVersionString = '1234';
-    
+    private $testCurrentVersion = '1234';
+
     /**
      * @var ContextVersion
      */
     private $contextVersion;
 
-    protected function setUp()
+    /**
+     * @return HttpRequest|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private function createStubRequestWithRequestedVersion(string $version): HttpRequest
     {
-        /** @var DataVersion|\PHPUnit_Framework_MockObject_MockObject $stubDataVersion */
-        $stubDataVersion = $this->createMock(DataVersion::class);
-        $stubDataVersion->method('__toString')->willReturn($this->testVersionString);
-        $this->contextVersion = new ContextVersion($stubDataVersion);
+        $stubRequest = $this->createMock(HttpRequest::class);
+        $stubRequest->method('hasQueryParameter')->with(ContextVersion::DATA_VERSION_REQUEST_PARAM)->willReturn(true);
+        $stubRequest->method('getQueryParameter')->with(ContextVersion::DATA_VERSION_REQUEST_PARAM)->willReturn($version);
+
+        return $stubRequest;
     }
 
-    public function testItIsAContextPartBuilder()
+    protected function setUp()
+    {
+        /** @var DataVersion|\PHPUnit_Framework_MockObject_MockObject $stubCurrentDataVersion */
+        $stubCurrentDataVersion = $this->createMock(DataVersion::class);
+        $stubCurrentDataVersion->method('__toString')->willReturn($this->testCurrentVersion);
+        $this->contextVersion = new ContextVersion($stubCurrentDataVersion);
+    }
+
+    public function testIsAContextPartBuilder()
     {
         $this->assertInstanceOf(ContextPartBuilder::class, $this->contextVersion);
     }
 
-    public function testItReturnsTheCode()
+    public function testReturnsTheCode()
     {
         $this->assertSame(DataVersion::CONTEXT_CODE, $this->contextVersion->getCode());
     }
 
-    public function testItReturnsTheVersionFromTheInputArrayIfPresent()
+    public function testReturnsTheVersionFromTheInputArrayIfPresent()
     {
         $inputDataSet = [DataVersion::CONTEXT_CODE => '1.0'];
         $this->assertSame('1.0', $this->contextVersion->getValue($inputDataSet));
     }
 
-    public function testItReturnsTheInjectedDataVersionValueIfTheInputContainsNoVersion()
+    public function testReturnsTheVersionFromTheRequestParametersIfPresent()
+    {
+        $version = 'foo';
+        $inputDataSet = [ContextBuilder::REQUEST => $this->createStubRequestWithRequestedVersion($version)];
+        $this->assertSame($version, $this->contextVersion->getValue($inputDataSet));
+    }
+
+    public function testReturnsTheInjectedDataVersionValueIfTheInputContainsNoVersion()
     {
         $inputDataSet = [];
-        $this->assertSame($this->testVersionString, $this->contextVersion->getValue($inputDataSet));
+        $this->assertSame($this->testCurrentVersion, $this->contextVersion->getValue($inputDataSet));
+    }
+
+    public function testTheVersionFromTheInputArrayHasTheHighestPriority()
+    {
+        $inputDataSet = [
+            DataVersion::CONTEXT_CODE => 'foo',
+            ContextBuilder::REQUEST   => $this->createStubRequestWithRequestedVersion('bar'),
+        ];
+        $this->assertSame('foo', $this->contextVersion->getValue($inputDataSet));
     }
 }
