eb407046c6a2c398bb65cc2cd429c3afef702a52
Fabian Blechschmidt <blechschmidt@fabian-blechschmidt.de>
2017-05-18 19:03:09 +0200

    Issue #1043: Refactor Catalog Import
    Implement Integration test for Product Import

--- tests/Integration/Suites/CatalogImportApiTest.php
+++ tests/Integration/Suites/CatalogImportApiTest.php
@@ -1,6 +1,6 @@
 <?php
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 namespace LizardsAndPumpkins;
 
@@ -9,13 +9,14 @@ use LizardsAndPumpkins\Http\HttpRequest;
 use LizardsAndPumpkins\Http\HttpRequestBody;
 use LizardsAndPumpkins\Http\HttpUrl;
 use LizardsAndPumpkins\Import\ImportCatalogCommand;
+use LizardsAndPumpkins\Import\Product\UpdateProductCommand;
 use LizardsAndPumpkins\Messaging\MessageReceiver;
 use LizardsAndPumpkins\Messaging\Queue;
 use LizardsAndPumpkins\Messaging\Queue\Message;
 
 class CatalogImportApiTest extends AbstractIntegrationTest
 {
-    private function getNextMessageFromQueue(Queue $queue): Message
+    private function getNextMessageFromQueue(Queue $queue) : Message
     {
         $receiver = new class implements MessageReceiver
         {
@@ -80,6 +81,49 @@ class CatalogImportApiTest extends AbstractIntegrationTest
 
         $message = $this->getNextMessageFromQueue($commandQueue);
         $this->assertSame(ImportCatalogCommand::CODE, $message->getName());
+        $this->assertSame($testDataVersionString, $message->getMetadata()['data_version']);
+
+        $this->assertSame(202, $response->getStatusCode());
+        $this->assertSame('', $response->getBody());
+    }
+
+    public function testV1ProductImportApiV1PutRequestHandler()
+    {
+        $testDataVersionString = 'foo-123';
+        $httpUrl = HttpUrl::fromString('http://example.com/api/product_import');
+        $httpHeaders = HttpHeaders::fromArray([
+            'Accept' => 'application/vnd.lizards-and-pumpkins.product_import.v1+json',
+        ]);
+
+        $productJson = json_encode([
+            'sku'        => 'led-arm-signallampe',
+            'type'       => 'simple',
+            'tax_class'  => 'class-1',
+            'attributes' => [
+                'backorders'  => true,
+                'url_key'     => 'led-arm-signallampe',
+                'description' => 'LED Arm-Signallampe<br />
+<br />
+LED Arm-Signallampe mit elastischem Band und Flasher mit variabler Blinkfolge,
+Flasher abnehmbar.',
+            ],
+        ]);
+
+        $httpRequestBodyString = json_encode(['product_data' => $productJson, 'dataVersion' => $testDataVersionString]);
+        $httpRequestBody = new HttpRequestBody($httpRequestBodyString);
+        $request = HttpRequest::fromParameters(HttpRequest::METHOD_PUT, $httpUrl, $httpHeaders, $httpRequestBody);
+
+        $factory = $this->prepareIntegrationTestMasterFactoryForRequest($request);
+        $implementationSpecificFactory = $this->getIntegrationTestFactory($factory);
+
+        $commandQueue = $factory->getCommandMessageQueue();
+        $this->assertEquals(0, $commandQueue->count());
+
+        $website = new InjectableDefaultWebFront($request, $factory, $implementationSpecificFactory);
+        $response = $website->processRequest();
+
+        $message = $this->getNextMessageFromQueue($commandQueue);
+        $this->assertSame(UpdateProductCommand::CODE, $message->getName());
         $this->assertSame($testDataVersionString, $message->getMetadata()['data_version']);
 
         $this->assertSame(202, $response->getStatusCode());
