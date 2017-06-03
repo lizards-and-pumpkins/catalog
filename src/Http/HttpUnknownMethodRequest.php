3300fb898cb500911c12b1c8a76e21877097240f
Vinai Kopp <vinai@netzarbeiter.com>
2017-06-03 17:41:11 +0200

    Issue #1067: Make setter protected

--- src/Http/HttpUnknownMethodRequest.php
+++ src/Http/HttpUnknownMethodRequest.php
@@ -7,13 +7,13 @@ namespace LizardsAndPumpkins\Http;
 class HttpUnknownMethodRequest extends HttpRequest
 {
     private $methodCode = '';
-    
+
     public function getMethod(): string
     {
         return $this->methodCode;
     }
 
-    public function setMethodCode(string $requestMethodCode)
+    final protected function setMethodCode(string $requestMethodCode)
     {
         $this->methodCode = strtoupper($requestMethodCode);
     }
